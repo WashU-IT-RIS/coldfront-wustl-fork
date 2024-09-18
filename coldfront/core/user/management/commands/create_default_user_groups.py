c9ef1c93c2b6f7ad49272041b4695990a986a60f
Gunnar Smith <gunnar@wustl.edu>
2024-09-18 11:42:39 -0500

    create simple python script to load default user groups

---
+++ coldfront/core/user/management/commands/create_default_user_groups.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+
+from django.contrib.auth.models import Group
+
+
+class Command(BaseCommand):
+    help = "Create default user groups"
+
+    def handle(self, *args, **options):
+        groups = ["RIS-UserSupport"]
+        for group in groups:
+            Group.objects.get_or_create(name=group)
+        print("Finished creating default user groups")
