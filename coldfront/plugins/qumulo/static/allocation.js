fdb1a076c71d5552afc150c14df0c9bb8f4148b9
John Prewitt <japrewit@wustl.edu>
2024-09-19 22:39:59 -0500

    Explicitly showing hidden flag

--- coldfront/plugins/qumulo/static/allocation.js
+++ coldfront/plugins/qumulo/static/allocation.js
@@ -25,6 +25,7 @@ submitButton.addEventListener("click", (event) => {
   const id_project_pk_elem = document.getElementById("id_project_pk");
   const smb = protocols.find((protocol) => protocol.value === "smb");
   console.log("id_project_pk_elem", id_project_pk_elem);
+  console.log("id_project_pk_elem.hidden", id_project_pk_elem.hidden);
   console.log("smb", smb.checked);
   console.log("confirmed", confirmed);
   // NOTE: we're using id_project_pk to determine whether we are on a
