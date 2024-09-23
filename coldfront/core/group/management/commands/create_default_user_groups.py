4d371d57918d9f97615ccdec0af7999dd425bd1a
Gunnar Smith <gunnar@wustl.edu>
2024-09-23 10:18:29 -0500

    commit before sharing with John

--- coldfront/core/group/management/commands/create_default_user_groups.py
+++ coldfront/core/group/management/commands/create_default_user_groups.py
@@ -1,14 +1,20 @@
 from django.core.management.base import BaseCommand
 
 from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission
+
+DEFAULT_RIS_USER_GROUPS = [{"name": "RIS-UserSupport", "permissions": []}]
 
 
 class Command(BaseCommand):
+    """
+    Command to create default user groups.
+    """
+
     help = "Create default user groups"
 
     def handle(self, *args, **options):
         print("Creating default user groups ...")
-        groups = ["RIS-UserSupport"]
-        for group in groups:
-            Group.objects.get_or_create(name=group)
+        for group in DEFAULT_RIS_USER_GROUPS:
+            Group.objects.get_or_create(name=group["name"])
         print("Finished creating default user groups")
