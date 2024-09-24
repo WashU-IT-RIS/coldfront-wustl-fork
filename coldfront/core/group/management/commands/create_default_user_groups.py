03756edca55eeb6d0fb767abaa7d0024e537023f
Gunnar Smith <gunnar@wustl.edu>
2024-09-24 15:38:52 -0500

    replace for-loop with queryset

--- coldfront/core/group/management/commands/create_default_user_groups.py
+++ coldfront/core/group/management/commands/create_default_user_groups.py
@@ -197,11 +197,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Creating default user groups ...")
         for group in DEFAULT_RIS_USER_GROUPS:
-            print(f"Creating group: {group['name']}")
-            Group.objects.get_or_create(name=group["name"])
-            for permission in group["permissions"]:
-                print(f"Adding permission: {permission['codename']}")
-                group_obj = Group.objects.get(name=group["name"])
-                group_obj.permissions.add(Permission.objects.get(id=permission["id"]))
+            group_obj = Group.objects.get_or_create(name=group["name"])
+            permissions = Permission.objects.filter(
+                codename__in=[p["codename"] for p in group["permissions"]]
+            )
+            group_obj.permissions.add(permissions)
 
         print("Finished creating default user groups")
