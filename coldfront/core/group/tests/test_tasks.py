7aad5ee0845b9fa530ecda13e332112cad0079f8
Gunnar Smith <gunnar@wustl.edu>
2024-09-30 15:14:21 -0500

    more typos

--- coldfront/core/group/tests/test_tasks.py
+++ coldfront/core/group/tests/test_tasks.py
@@ -20,27 +20,27 @@ class GrantUserSupportGlobalProjectManagerTest(TestCase):
         # Create a group and three users
         self.group = Group.objects.get_or_create(name="RIS-UserSupport", id=1).first()
         # Create two users as PIs
-        user1 = User.objects.get_or_create(username="user1", id=1).first()
-        user2 = User.objects.get_or_create(username="user2", id=2).first()
+        user1 = User.objects.get_or_create(username="user1", id=1)[0]
+        user2 = User.objects.get_or_create(username="user2", id=2)[0]
         self.user1 = user1
         self.user2 = user2
         # Create a user that is not a PI
-        user3 = User.objects.get_or_create(username="user3", id=3).first()
+        user3 = User.objects.get_or_create(username="user3", id=3)[0]
         self.user3 = user3
 
         # Create two projects with separate PIs
         project1 = Project.objects.get_or_create(
             title="Project1", id=1, pi=user1, status=project_status
-        )
+        )[0]
         project2 = Project.objects.get_or_create(
             title="Project2", id=2, pi=user2, status=project_status
-        )
+        )[0]
         self.project1 = project1
         self.project2 = project2
 
-        self.role_choice = ProjectUserRoleChoice.objects.get_or_create(
-            name="Manager"
-        ).first()
+        self.role_choice = ProjectUserRoleChoice.objects.get_or_create(name="Manager")[
+            0
+        ]
         self.status_choice = project_status
 
     # Test that the grant_usersupport_global_project_manager function works as expected
