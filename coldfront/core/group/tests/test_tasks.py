dadbcb0d74148cb1bf3135e6c3f019fe3288f045
Gunnar Smith <gunnar@wustl.edu>
2024-09-30 15:06:19 -0500

    more typos

--- coldfront/core/group/tests/test_tasks.py
+++ coldfront/core/group/tests/test_tasks.py
@@ -13,30 +13,34 @@ from coldfront.core.project.models import (
 
 class GrantUserSupportGlobalProjectManagerTest(TestCase):
     def setUp(self):
-        project_status = ProjectUserStatusChoice.objects.get_or_create(name="Active")
+        project_status = ProjectUserStatusChoice.objects.get_or_create(
+            name="Active"
+        ).first()
 
         # Create a group and three users
-        self.group = Group.objects.get_or_create(name="RIS-UserSupport", id=1)
+        self.group = Group.objects.get_or_create(name="RIS-UserSupport", id=1).first()
         # Create two users as PIs
-        user1 = User.objects.get_or_create(username="user1", id=1)
-        user2 = User.objects.get_or_create(username="user2", id=2)
+        user1 = User.objects.get_or_create(username="user1", id=1).first()
+        user2 = User.objects.get_or_create(username="user2", id=2).first()
         self.user1 = user1
         self.user2 = user2
         # Create a user that is not a PI
-        user3 = User.objects.get_or_create(username="user3", id=3)
+        user3 = User.objects.get_or_create(username="user3", id=3).first()
         self.user3 = user3
 
         # Create two projects with separate PIs
         project1 = Project.objects.get_or_create(
-            title="Project1", id=1, pi=1, status=project_status
+            title="Project1", id=1, pi=user1, status=project_status
         )
         project2 = Project.objects.get_or_create(
-            title="Project2", id=2, pi=2, status=project_status
+            title="Project2", id=2, pi=user2, status=project_status
         )
         self.project1 = project1
         self.project2 = project2
 
-        self.role_choice = ProjectUserRoleChoice.objects.get_or_create(name="Manager")
+        self.role_choice = ProjectUserRoleChoice.objects.get_or_create(
+            name="Manager"
+        ).first()
         self.status_choice = project_status
 
     # Test that the grant_usersupport_global_project_manager function works as expected
