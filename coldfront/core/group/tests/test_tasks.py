503cd1755f51e7eab0c27acc0c877fe50cdb50d0
Gunnar Smith <gunnar@wustl.edu>
2024-09-30 10:45:01 -0500

    test

--- coldfront/core/group/tests/test_tasks.py
+++ coldfront/core/group/tests/test_tasks.py
@@ -13,22 +13,24 @@ from coldfront.core.project.models import (
 
 class GrantUserSupportGlobalProjectManagerTest(TestCase):
     def setUp(self):
-        self.group = Group.objects.create(name="RIS-UserSupport")
-        self.user1 = User.objects.create(username="user1")
-        self.user2 = User.objects.create(username="user2")
+        self.group = Group.objects.get_or_create(name="RIS-UserSupport")
+        self.user1 = User.objects.get_or_create(username="user1")
+        self.user2 = User.objects.get_or_create(username="user2")
         self.group.user_set.add(self.user1, self.user2)
 
-        self.project1 = Project.objects.create(name="Project 1")
-        self.project2 = Project.objects.create(name="Project 2")
+        self.project1 = Project.objects.get_or_create(name="Project 1")
+        self.project2 = Project.objects.get_or_create(name="Project 2")
 
-        self.role_choice = ProjectUserRoleChoice.objects.create(name="Manager")
-        self.status_choice = ProjectUserStatusChoice.objects.create(name="Active")
+        self.role_choice = ProjectUserRoleChoice.objects.get_or_create(name="Manager")
+        self.status_choice = ProjectUserStatusChoice.objects.get_or_create(
+            name="Active"
+        )
 
     def test_grant_usersupport_global_project_manager(self):
         grant_usersupport_global_project_manager()
 
-        for project in [self.project1, self.project2]:
-            for user in [self.user1, self.user2]:
+        for project, pb in [self.project1, self.project2]:
+            for user, ub in [self.user1, self.user2]:
                 project_user = ProjectUser.objects.get(project=project, user=user)
                 self.assertEqual(project_user.role, self.role_choice)
                 self.assertEqual(project_user.status, self.status_choice)
