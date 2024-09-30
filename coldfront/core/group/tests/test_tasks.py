0709c86c5dd876277ccea7e61efbd988997ad39d
Gunnar Smith <gunnar@wustl.edu>
2024-09-30 14:58:35 -0500

    typos

--- coldfront/core/group/tests/test_tasks.py
+++ coldfront/core/group/tests/test_tasks.py
@@ -13,22 +13,31 @@ from coldfront.core.project.models import (
 
 class GrantUserSupportGlobalProjectManagerTest(TestCase):
     def setUp(self):
+        project_status = ProjectUserStatusChoice.objects.get_or_create(name="Active")
+
         # Create a group and three users
         self.group = Group.objects.get_or_create(name="RIS-UserSupport", id=1)
         # Create two users as PIs
-        self.user1 = User.objects.get_or_create(username="user1", id=1)
-        self.user2 = User.objects.get_or_create(username="user2", id=2)
+        user1 = User.objects.get_or_create(username="user1", id=1)
+        user2 = User.objects.get_or_create(username="user2", id=2)
+        self.user1 = user1
+        self.user2 = user2
         # Create a user that is not a PI
-        self.user3 = User.objects.get_or_create(username="user3", id=3)
+        user3 = User.objects.get_or_create(username="user3", id=3)
+        self.user3 = user3
 
         # Create two projects with separate PIs
-        self.project1 = Project.objects.get_or_create(title="Project 1", id=1, pi_id=1)
-        self.project2 = Project.objects.get_or_create(title="Project 2", id=2, pi_id=2)
+        project1 = Project.objects.get_or_create(
+            title="Project1", id=1, pi=user1, status=project_status
+        )
+        project2 = Project.objects.get_or_create(
+            title="Project2", id=2, pi=user2, status=project_status
+        )
+        self.project1 = project1
+        self.project2 = project2
 
         self.role_choice = ProjectUserRoleChoice.objects.get_or_create(name="Manager")
-        self.status_choice = ProjectUserStatusChoice.objects.get_or_create(
-            name="Active"
-        )
+        self.status_choice = project_status
 
     # Test that the grant_usersupport_global_project_manager function works as expected
     def test_grant_usersupport_global_project_manager(self):
