1fabd8841b00155d07efc535457509f3b832a066
Gunnar Smith <gunnar@wustl.edu>
2024-10-07 10:04:32 -0500

    simplify test_tasks.py setup

--- coldfront/core/group/tests/test_tasks.py
+++ coldfront/core/group/tests/test_tasks.py
@@ -27,44 +27,45 @@ class GrantUserSupportGlobalProjectManagerTest(TestCase):
         self.group, _ = Group.objects.get_or_create(name="RIS-UserSupport")
         # Define user attributes
         user_attributes = [
-            {"username": "user1"},
-            {"username": "user2"},
-            {"username": "user3"},
+            {"username": "user1", "id": 1},
+            {"username": "user2", "id": 2},
+            {"username": "user3", "id": 3},
         ]
-        # Create users and store them in an array
+        # Create users and add them to the group
         self.users = []
         for attrs in user_attributes:
             user, _ = User.objects.get_or_create(**attrs)
+            user.groups.add(self.group)
             self.users.append(user)
-            # Add the user to the group
-            index = self.users.index(user)
-            self.users[index].groups.add(self.group)
 
-        # Create two projects with separate PIs
-        self.project1, _ = Project.objects.get_or_create(
-            title="Project1",
-            pi=self.users[1],
-            status=self.project_status,
-            field_of_science=field_of_science,
-        )
-        self.project2, _ = Project.objects.get_or_create(
-            title="Project2",
-            pi=self.users[2],
-            status=self.project_status,
-            field_of_science=field_of_science,
-        )
+        # Create two projects
+        self.projects = []
+        project_attributes = [
+            {"title": "Project1", "pi": self.users[1]},
+            {"title": "Project2", "pi": self.users[2]},
+        ]
+        for attrs in project_attributes:
+            project, _ = Project.objects.get_or_create(
+                status=self.project_status,
+                field_of_science=field_of_science,
+                **attrs,
+            )
+            self.projects.append(project)
 
         self.projectuser_role, _ = ProjectUserRoleChoice.objects.get_or_create(
             name="Manager"
         )
 
+    # POSITIVE TESTS
     # Test that the grant_usersupport_global_project_manager function works as expected
     def test_grant_usersupport_global_project_manager(self):
         grant_usersupport_global_project_manager()
 
-        for project in [self.project1, self.project2]:
+        for project in self.projects:
             for user in self.users:
                 project_user = ProjectUser.objects.get(project=project, user=user)
                 self.assertEqual(project_user.role, self.projectuser_role)
                 self.assertEqual(project_user.status, self.projectuser_status)
                 self.assertTrue(project_user.enable_notifications)
+
+    # NEGATIVE TESTS
