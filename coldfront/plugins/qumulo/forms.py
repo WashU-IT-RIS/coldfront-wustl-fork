b9b34510f7f0eed5655b145beca0da0c123c7133
Caroline Jordan <jcaroline@wustl.edu>
2024-10-10 19:10:33 -0700

    can only select one billing cycle option

--- coldfront/plugins/qumulo/forms.py
+++ coldfront/plugins/qumulo/forms.py
@@ -64,8 +64,7 @@ class AllocationForm(forms.Form):
         validators=[validate_single_ad_user],
         required=False,
     )
-    billing_cycle = forms.MultipleChoiceField(
-        widget=forms.CheckboxSelectMultiple(),
+    billing_cycle = forms.ChoiceField(
         choices=BILLING_CYCLE_OPTIONS,
         label="Billing Cycle Options",
         help_text="Choose one billing cycle option from the above list",
