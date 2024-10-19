49774f6b68299d14263c5e30d05e88ae479c88c5
John Prewitt <japrewit@wustl.edu>
2024-10-19 14:45:01 -0500

    PR findings - consolidating attribute type names into a constant and re-organizing the JSON type validation error message

--- coldfront/core/constants.py
+++ coldfront/core/constants.py
@@ -1,1 +1,13 @@
-BILLING_CYCLE_OPTIONS = ['monthly', 'prepaid']
+BILLING_CYCLE_OPTIONS = ["monthly", "prepaid"]
+
+ALL_ATTRIBUTE_TYPES = [
+    "Date",
+    "Float",
+    "Int",
+    "Text",
+    "Yes/No",
+    "No",
+    "Attribute Expanded Text",
+    "JSON",
+    "BillingCycle",
+]
