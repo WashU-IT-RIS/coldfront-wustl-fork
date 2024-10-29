63b1dad0b2d91a231e4b0a727c371826fbd3c5ca
Caroline Jordan <jcaroline@wustl.edu>
2024-10-29 10:24:52 -0700

    breaking on purpose

--- coldfront/plugins/qumulo/signals.py
+++ coldfront/plugins/qumulo/signals.py
@@ -57,7 +57,9 @@ def on_allocation_activate(sender, **kwargs):
             (begin_date.month + prepaid_months - 1) % 12 + 1,
             begin_date.day,
         )
+        print(exp)
         print(prepaid_until)
+        prepaid_until.value = 0
     else:
         prepaid_until = None
 
