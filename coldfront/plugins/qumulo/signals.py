819441cb7718e3fdd98b209384eb46785ce11aeb
Caroline Jordan <jcaroline@wustl.edu>
2024-10-29 12:22:05 -0700

    objects

--- coldfront/plugins/qumulo/signals.py
+++ coldfront/plugins/qumulo/signals.py
@@ -60,7 +60,7 @@ def on_allocation_activate(sender, **kwargs):
     else:
         prepaid_until = None
 
-    AllocationAttribute.get_or_create(
+    AllocationAttribute.objects.get_or_create(
         allocation_attribute_type=datetime,
         allocation="prepaid_expiration",
         value=prepaid_until,
