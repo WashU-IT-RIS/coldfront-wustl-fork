eb2e39aa482a1e8ce964ebdf2a065b7ae212de32
Caroline Jordan <jcaroline@wustl.edu>
2024-11-05 22:25:23 -0800

    back to daily schedule

--- coldfront/plugins/qumulo/management/commands/add_scheduled_ad_poller.py
+++ coldfront/plugins/qumulo/management/commands/add_scheduled_ad_poller.py
@@ -26,8 +26,7 @@ class Command(BaseCommand):
         Schedule.objects.get_or_create(
             func="coldfront.plugins.qumulo.management.commands.add_scheduled_ad_poller.daily_billing_status_check",
             name="Check Billing Statuses",
-            schedule_type=Schedule.MINUTES,
-            minutes=1,
+            schedule_type=Schedule.DAILY,
             repeats=-1,
         )
 
