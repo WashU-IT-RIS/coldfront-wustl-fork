910465324bc5bd1a28f8410b3e64957c13198bd3
Caroline Jordan <jcaroline@wustl.edu>
2024-11-07 09:55:20 -0800

    values

--- coldfront/plugins/qumulo/tests/test_signals.py
+++ coldfront/plugins/qumulo/tests/test_signals.py
@@ -121,7 +121,7 @@ class TestSignals(TestCase):
             allocation=self.storage_allocation,
         )
 
-        self.assertEqual(prepaid_exp, datetime.today().strftime("%Y-%m-%d"))
+        self.assertEqual(prepaid_exp.value, datetime.today().strftime("%Y-%m-%d"))
 
     def test_allocation_change_approved_updates_allocation(
         self,
