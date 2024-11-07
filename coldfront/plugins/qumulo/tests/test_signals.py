ca9468694ca07adb0c9dd1cf0aeb5f6c0484ef22
Caroline Jordan <jcaroline@wustl.edu>
2024-11-07 09:11:17 -0800

    testing

--- coldfront/plugins/qumulo/tests/test_signals.py
+++ coldfront/plugins/qumulo/tests/test_signals.py
@@ -111,10 +111,12 @@ class TestSignals(TestCase):
         allocation_activate.send(
             sender=self.__class__, allocation_pk=self.storage_allocation.pk
         )
+        print(self)
         prepaid_exp = AllocationAttribute.objects.get(
             allocation_attribute_type="prepaid_expiration",
             allocation=self.storage_allocation,
         )
+        print(prepaid_exp)
         self.assertEqual(prepaid_exp, datetime.today().strftime("%Y-%m-%d"))
 
     def test_allocation_change_approved_updates_allocation(
