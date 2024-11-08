e5f6a6aaeb5bb82847d2c659bcdae275cd5199fc
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2024-11-08 10:21:34 -0400

    ITDEV-35274 minor update

--- coldfront/plugins/qumulo/services/itsm/itsm_client.py
+++ coldfront/plugins/qumulo/services/itsm/itsm_client.py
@@ -49,7 +49,7 @@ itsm_client = ItsmClient()
 itsm_client.get_fs1_allocation_by_fileset_name("jin810_active")
 itsm_client.get_fs1_allocation_by_fileset_name("not_going_to_be_found")
 itsm_client.get_fs1_allocation_by_fileset_name()
-itsm_client.get_fs1_allocation_by_fileset_alias("jin810_active")
+itsm_client.get_fs1_allocation_by_fileset_alias("halllab")
 itsm_client.get_fs1_allocation_by_fileset_alias("not_going_to_be_found")
 itsm_client.get_fs1_allocation_by_fileset_alias()
 """
