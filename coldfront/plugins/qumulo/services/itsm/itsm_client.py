099f75ede865aad28689c88e7c8e17019a286b32
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2024-11-08 10:16:48 -0400

    ITDEV-35274 update itsm client; provides two entry points by fileset_name and by fileset_alias

--- coldfront/plugins/qumulo/services/itsm/itsm_client.py
+++ coldfront/plugins/qumulo/services/itsm/itsm_client.py
@@ -19,16 +19,23 @@ class ItsmClient:
         itsm_fields = ",".join(itsm_attributes)
         self.url = f"{protocol}://{host}:{port}/rest/attr/info/service_provision?attribute={itsm_fields}"
 
-    def get_fs1_allocation(self, fileset_name) -> str:
-        filtered_url = self.__get_filtered_url(fileset_name)
+    def get_fs1_allocation_by_fileset_name(self, fileset_name) -> str:
+        return self.__get_fs1_allocation_by("fileset_name", fileset_name)
+    
+    def get_fs1_allocation_by_fileset_alias(self, fileset_alias) -> str:
+        return self.__get_fs1_allocation_by("fileset_alias", fileset_alias)
+    
+    #### PRIVATE METHODS ####
+    def __get_fs1_allocation_by(self, fileset_key, fileset_value) -> str:
+        filtered_url = self.__get_filtered_url(fileset_key, fileset_value)
         response = requests.get(filtered_url, headers=self.__get_headers())
         response.raise_for_status()
 
         data = response.json().get("data")
         return data
 
-    def __get_filtered_url(self, fileset_name) -> str:
-        filters = f'filter={{"fileset_alias":"{fileset_name}"}}'
+    def __get_filtered_url(self, fileset_key, fileset_value) -> str:
+        filters = f'filter={{"{fileset_key}":"{fileset_value}"}}'
         return f"{self.url}&{filters}"
 
     def __get_headers(self) -> dict:
@@ -39,29 +46,10 @@ class ItsmClient:
 For manual testing
 from coldfront.plugins.qumulo.services.itsm.itsm_client import ItsmClient
 itsm_client = ItsmClient()
-itsm_client.get_fs1_allocation("jin810_active")
-itsm_client.get_fs1_allocation("not_going_to_be_found")
-itsm_client.get_fs1_allocation() 
-"""
-
-"""
-# TODO Delete
-
-fileset_name = "jin810_active"
-import os
-import requests
-from dotenv import load_dotenv
-from coldfront.plugins.qumulo.services.itsm.allocation_fields_factory import (
-    itsm_attributes,
-)
-protocol = os.environ.get("ITSM_PROTOCOL") or "http"
-host = os.environ.get("ITSM_HOST") or "localhost"
-port = os.environ.get("ITSM_PORT") or "3000"
-user = os.environ.get("ITSM_USER") or "sleong"
-itsm_fields = ",".join(itsm_attributes)
-url = f"{protocol}://{host}:{port}/rest/attr/info/service_provision?attribute={itsm_fields}"
-headers = {"x-remote-user": user, "content-type": "application/json"}
-filters = f'filter={{"fileset_alias":"{fileset_name}"}}'
-filtered_url = f"{url}&{filters}"
-response = requests.get(filtered_url, headers=headers)
+itsm_client.get_fs1_allocation_by_fileset_name("jin810_active")
+itsm_client.get_fs1_allocation_by_fileset_name("not_going_to_be_found")
+itsm_client.get_fs1_allocation_by_fileset_name()
+itsm_client.get_fs1_allocation_by_fileset_alias("jin810_active")
+itsm_client.get_fs1_allocation_by_fileset_alias("not_going_to_be_found")
+itsm_client.get_fs1_allocation_by_fileset_alias()
 """
