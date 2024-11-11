7d0d0166b1d76e825ee8df3b3d723a782b1d7934
Caroline Jordan <jcaroline@wustl.edu>
2024-11-11 15:23:13 -0800

    mock

--- coldfront/plugins/qumulo/tests/test_signals.py
+++ coldfront/plugins/qumulo/tests/test_signals.py
@@ -128,6 +128,7 @@ class TestSignals(TestCase):
             "Can't create allocation: Some attributes are missing or invalid"
         )
 
+    @patch("coldfront.plugins.qumulo.signals.async_task")
     def test_allocation_activates_calculates_prepaid_expiration_monthly(
         self,
         mock_ACL_ActiveDirectoryApi: MagicMock,
@@ -148,6 +149,7 @@ class TestSignals(TestCase):
 
         self.assertEqual(prepaid_exp.value, datetime.today().strftime("%Y-%m-%d"))
 
+    @patch("coldfront.plugins.qumulo.signals.async_task")
     def test_allocation_activates_calculates_prepaid_expiration_prepaid(
         self,
         mock_ACL_ActiveDirectoryApi: MagicMock,
