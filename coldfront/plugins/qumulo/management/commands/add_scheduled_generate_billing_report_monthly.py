9a07ec692c763f16af124f4ce4c91f5a0507451e
Tz-Kai Kevin Lin <tz-kai.lin@wustl.edu>
2024-11-12 08:35:56 -0600

    ITDEV-35120; updated the scheduled time; tested the issue with the missing successful active test deployment;

--- coldfront/plugins/qumulo/management/commands/add_scheduled_generate_billing_report_monthly.py
+++ coldfront/plugins/qumulo/management/commands/add_scheduled_generate_billing_report_monthly.py
@@ -7,8 +7,8 @@ from django_q.models import Schedule
 
 from coldfront.plugins.qumulo.utils.eib_billing import EIBBilling
 
-SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_5_30_AM = (
-    arrow.utcnow().replace(day=2, hour=5, minute=30).format(arrow.FORMAT_RFC3339)
+SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_6_30_AM = (
+    arrow.utcnow().replace(day=2, hour=6, minute=30).format(arrow.FORMAT_RFC3339)
 )
 
 
@@ -20,7 +20,7 @@ class Command(BaseCommand):
             func="coldfront.plugins.qumulo.management.commands.add_scheduled_generate_billing_report_monthly.generate_storage2_monthly_billing_report",
             name="Generate Storage2 Monthly Billing Report",
             schedule_type=Schedule.MONTHLY,
-            next_run=SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_5_30_AM,
+            next_run=SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_6_30_AM,
         )
 
 
