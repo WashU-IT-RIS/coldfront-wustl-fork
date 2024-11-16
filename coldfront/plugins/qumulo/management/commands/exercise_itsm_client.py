48fe49b1625b1dd3e3d9bf9214c59a744c74b18f
John Prewitt <japrewit@wustl.edu>
2024-11-15 20:59:21 -0600

    Adding coldfront command

---
+++ coldfront/plugins/qumulo/management/commands/exercise_itsm_client.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+from coldfront.plugins.qumulo.services.itsm.itsm_client import ItsmClient
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print("Manually exercising ITSM client")
+        itsm_client = ItsmClient()
+        import pdb
+        pdb.set_trace()
+        result = itsm_client.get_fs1_allocation_by_fileset_name("jin810_active")
+        pdb.set_trace()
+        # itsm_client.get_fs1_allocation_by_fileset_name("not_going_to_be_found")
+        # itsm_client.get_fs1_allocation_by_fileset_name()
+        # itsm_client.get_fs1_allocation_by_fileset_alias("halllab")
+        # itsm_client.get_fs1_allocation_by_fileset_alias("not_going_to_be_found")
+        # itsm_client.get_fs1_allocation_by_fileset_alias()
