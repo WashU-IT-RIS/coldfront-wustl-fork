efa7eadf115676f6fe0fbb4cf88d78102e625edb
Caroline Jordan <jcaroline@wustl.edu>
2024-11-20 11:11:52 -0800

    removal of form-group

--- coldfront/plugins/qumulo/static/allocation.js
+++ coldfront/plugins/qumulo/static/allocation.js
@@ -97,7 +97,7 @@ function handlePrepaidCycleSelection(event) {
 
 function handlePrepaidBillingDateDisplay(){
   const bill_cycle_value = document.getElementById("id_billing_cycle").value;
-  const prepaid_billing_date = document.getElementById("div_id_prepaid_billing_date.form-group");
+  const prepaid_billing_date = document.getElementById("div_id_prepaid_billing_date");
   console.log("handlePrepaidBillingDateDisplay");
 
   if (bill_cycle_value !== "prepaid") {
