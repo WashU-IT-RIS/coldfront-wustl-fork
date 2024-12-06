57d1f9ede476aa19c785db4cc837fb56f8a8eef4
Caroline Jordan <jcaroline@wustl.edu>
2024-12-06 15:13:57 -0800

    removing comment

--- coldfront/plugins/qumulo/signals.py
+++ coldfront/plugins/qumulo/signals.py
@@ -60,7 +60,7 @@ def on_allocation_activate(sender, **kwargs):
     prepaid_billing_start = allocation.get_attribute(name="prepaid_billing_date")
     if bill_cycle == "prepaid":
         prepaid_months = allocation.get_attribute(name="prepaid_time")
-        # prepaid_billing_start = datetime.strptime(prepaid_billing_start, "%Y-%m-%d")
+        prepaid_billing_start = datetime.strptime(prepaid_billing_start, "%Y-%m-%d")
         prepaid_until = datetime(
             prepaid_billing_start.year
             + (prepaid_billing_start.month + prepaid_months - 1) // 12,
