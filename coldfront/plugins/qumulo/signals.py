94bf7168e2c3d5959bcd80ec05d4083727b1186f
Caroline Jordan <jcaroline@wustl.edu>
2024-12-06 15:18:03 -0800

    is prepaid_billing_date retrieved correctly

--- coldfront/plugins/qumulo/signals.py
+++ coldfront/plugins/qumulo/signals.py
@@ -57,7 +57,10 @@ def on_allocation_activate(sender, **kwargs):
     allocation_attribute_obj_type = AllocationAttributeType.objects.get(
         name="prepaid_expiration"
     )
-    prepaid_billing_start = allocation.get_attribute(name="prepaid_billing_date")
+    # prepaid_billing_start = allocation.get_attribute(name="prepaid_billing_date")
+    prepaid_billing_start = AllocationAttributeType.objects.get(
+        name="prepaid_billing_date", allocation=allocation
+    )
     if bill_cycle == "prepaid":
         prepaid_months = allocation.get_attribute(name="prepaid_time")
         prepaid_billing_start = datetime.strptime(prepaid_billing_start, "%Y-%m-%d")
