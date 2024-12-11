d3680c68010c564320d9d7c2cf51e335199763a2
Caroline Jordan <jcaroline@wustl.edu>
2024-12-10 16:16:12 -0800

    back in

--- coldfront/plugins/qumulo/management/commands/check_billing_cycles.py
+++ coldfront/plugins/qumulo/management/commands/check_billing_cycles.py
@@ -31,7 +31,7 @@ def calculate_prepaid_expiration(
     logger.warn(f"{prepaid_expiration}")
     if bill_cycle == "prepaid" and prepaid_expiration == None:
         logger.warn(f"Inside if statement")
-        # prepaid_billing_start = datetime.strptime(prepaid_billing_start, "%Y-%m-%d")
+        prepaid_billing_start = datetime.strptime(prepaid_billing_start, "%Y-%m-%d")
         prepaid_until = datetime(
             prepaid_billing_start.year
             + (prepaid_billing_start.month + prepaid_months - 1) // 12,
