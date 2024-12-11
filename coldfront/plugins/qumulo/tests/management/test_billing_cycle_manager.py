359b26c57cedc0d5261cd697bd91685c7d8ee4e7
Caroline Jordan <jcaroline@wustl.edu>
2024-12-11 14:50:46 -0800

    prepaid_present_experation_date

--- coldfront/plugins/qumulo/tests/management/test_billing_cycle_manager.py
+++ coldfront/plugins/qumulo/tests/management/test_billing_cycle_manager.py
@@ -57,27 +57,45 @@ class TestBillingCycleTypeUpdates(TestCase):
             "cost_center": "Uncle Pennybags",
             "department_number": "Time Travel Services",
             "service_rate": "general",
-            "billing_cycle": "monthly",
-        }
-        self.prepaid_future_form_data = {
-            "storage_filesystem_path": "foo",
-            "storage_export_path": "bar",
-            "storage_ticket": "ITSD-54321",
-            "storage_name": "baz",
-            "storage_quota": 7,
-            "protocols": ["nfs"],
-            "rw_users": ["test"],
-            "ro_users": ["test1"],
-            "cost_center": "Uncle Pennybags",
-            "department_number": "Time Travel Services",
-            "service_rate": "general",
             "billing_cycle": "prepaid",
-            "prepaid_time": 0,
+            "prepaid_time": 3,
+            "prepaid_billing_date": datetime.today(),
         }
 
     def prepaid_past_expiration_date(self):
         allocation = create_allocation(
             self.project, self.user, self.prepaid_past_form_data
+        )
+
+        prepaid_billing_start = self.prepaid_past_form_data["prepaid_billing_date"]
+        prepaid_months = self.prepaid_past_form_data["prepaid_time"]
+
+        calculate_prepaid_expiration(
+            allocation,
+            self.prepaid_past_form_data["billing_cycle"],
+            prepaid_months,
+            str(prepaid_billing_start),
+            None,
+        )
+        prepaid_billing_start = datetime.strptime(prepaid_billing_start, "%Y-%m-%d")
+        prepaid_until = datetime(
+            prepaid_billing_start.year
+            + (prepaid_billing_start.month + prepaid_months - 1) // 12,
+            (prepaid_billing_start.month + prepaid_months - 1) % 12 + 1,
+            prepaid_billing_start.day,
+        )
+        prepaid_expiration_attribute = AllocationAttributeType.objects.get(
+            name="prepaid_expiration"
+        )
+        prepaid_exp_value = AllocationAttribute.objects.get(
+            allocation=allocation,
+            allocation_attribute_type=prepaid_expiration_attribute,
+        ).value
+        self.assertEqual(prepaid_exp_value, str(prepaid_until))
+
+    def prepaid_present_expiration_date(self):
+        allocation = create_allocation(
+            self.project, self.user, self.prepaid_present_form_data
         )
 
         prepaid_billing_start = self.prepaid_past_form_data["prepaid_billing_date"]
