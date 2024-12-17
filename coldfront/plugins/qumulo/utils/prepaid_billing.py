c6686bd2fb7b812e4bbd2dc593b33b502b3bc905
Caroline Jordan <jcaroline@wustl.edu>
2024-12-17 15:17:46 -0800

    filter based on prepaid_billing_Date

--- coldfront/plugins/qumulo/utils/prepaid_billing.py
+++ coldfront/plugins/qumulo/utils/prepaid_billing.py
@@ -173,7 +173,7 @@ FROM (
         AND
           astatus.name = 'Active'
     ) AS data
-    WHERE billing_cycle = 'prepaid'
+    WHERE prepaid_billing_date = delivery_date
         AND exempt <> TRUE
 ) AS report 
 WHERE report.billing_amount > 0;
