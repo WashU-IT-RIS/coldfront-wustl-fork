ced448b54d5e01a3dc1eaf5c8616166182c2aa9b
Jim Harter <harterj@wustl.edu>
2024-12-18 01:45:23 -0600

    ok yeah cool its fixed now

--- coldfront/plugins/qumulo/tests/test_tasks.py
+++ coldfront/plugins/qumulo/tests/test_tasks.py
@@ -370,7 +370,9 @@ class TestAddMembersToADGroup(TestCase):
 
         mock_user_response = {
             "dn": "user_dn",
-            "objectClass": ["top", "person", "organizationalPerson", "user"],
+            "attributes": {
+                "objectClass": ["top", "person", "organizationalPerson", "user"]
+            },
         }
         active_directory_instance = MagicMock()
         active_directory_instance.get_member.return_value = mock_user_response
@@ -444,7 +446,9 @@ class TestAddMembersToADGroup(TestCase):
     ):
         mock_user_response = {
             "dn": "user_dn",
-            "objectClass": ["top", "person", "organizationalPerson", "user"],
+            "attributes": {
+                "objectClass": ["top", "person", "organizationalPerson", "user"]
+            },
         }
         active_directory_instance = MagicMock()
         active_directory_instance.get_member.return_value = mock_user_response
@@ -489,7 +493,7 @@ class TestAddMembersToADGroup(TestCase):
     ):
         mock_user_response = {
             "dn": "user_dn",
-            "objectClass": ["top", "group"],
+            "attributes": {"objectClass": ["top", "group"]},
         }
         active_directory_instance = MagicMock()
         active_directory_instance.get_member.return_value = mock_user_response
@@ -551,7 +555,9 @@ class TestAddMembersToADGroup(TestCase):
     def __get_member_mock(self, account_name: str, good_users: list[str]) -> dict:
         mock_member_response = {
             "dn": "user_dn",
-            "objectClass": ["top", "person", "organizationalPerson", "user"],
+            "attributes": {
+                "objectClass": ["top", "person", "organizationalPerson", "user"]
+            },
         }
         if account_name in good_users:
             return mock_member_response
@@ -635,7 +641,7 @@ class TestAddMembersToADGroup(TestCase):
         active_directory_instance = MagicMock()
         mock_member_response = {
             "dn": "user_dn",
-            "objectClass": ["top", "group"],
+            "attributes": {"objectClass": ["top", "group"]},
         }
         active_directory_instance.get_member.return_value = mock_member_response
         mock_active_directory_api.return_value = active_directory_instance
