521d49d1ceed253342bad90433f1be395a698cd8
Caroline Jordan <jcaroline@wustl.edu>
2024-12-23 16:16:20 -0800

    hm

--- coldfront/plugins/qumulo/static/allocation.js
+++ coldfront/plugins/qumulo/static/allocation.js
@@ -99,7 +99,7 @@ function handlePrepaidCycleSelection(event) {
 
 function handlePrepaidBillingDateDisplay(){
   const bill_cycle_value = document.getElementById("id_billing_cycle").value;
-  const prepaid_billing_date = document.getElementById("div_id_prepaid_billing_date");
+  const prepaid_billing_date = document.getElementById("id_prepaid_billing_date");
   const prepaid_billing_date_value = document.getElementById("id_prepaid_billing_date").value;
   const prepaid_billing_date_invalid = document.getElementById("error_1_id_prepaid_billing_date.invalid-feedback");
 
