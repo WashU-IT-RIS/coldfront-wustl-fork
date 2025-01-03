fff6cdd3dd5c22e965c3bb52f7d2f78442bbb6ab
John Prewitt <japrewit@wustl.edu>
2025-01-03 12:47:52 -0600

    adding some logging to post-save handlers

--- coldfront/core/user/signals.py
+++ coldfront/core/user/signals.py
@@ -8,10 +8,12 @@ from coldfront.core.user.models import UserProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    print("create_user_profile called")
     if created:
         UserProfile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+    print("save_user_profile called")
     instance.userprofile.save()
