c737cda6e046583167f9050ca96c3804a23c6ea4
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-01-27 16:50:30 -0400

    ITDEV-36272 mock AD API call in metadata tests

--- coldfront/plugins/qumulo/tests/services/itsm/test_migrate_to_coldfront.py
+++ coldfront/plugins/qumulo/tests/services/itsm/test_migrate_to_coldfront.py
@@ -78,10 +78,12 @@ class TestMigrateToColdfront(TestCase):
     @mock.patch(
         "coldfront.plugins.qumulo.services.itsm.migrate_to_coldfront.ItsmClient"
     )
+    @mock.patch("coldfront.plugins.qumulo.services.allocation_service.ActiveDirectoryAPI")
     @mock.patch("coldfront.plugins.qumulo.services.allocation_service.async_task")
     def test_migrate_to_coldfront_by_fileset_name(
         self,
         mock_async_task: mock.MagicMock,
+        mock_active_directory_api: mock.MagicMock,
         mock_itsm_client: mock.MagicMock,
     ) -> None:
         with open(
@@ -125,10 +127,12 @@ class TestMigrateToColdfront(TestCase):
     @mock.patch(
         "coldfront.plugins.qumulo.services.itsm.migrate_to_coldfront.ItsmClient"
     )
+    @mock.patch("coldfront.plugins.qumulo.services.allocation_service.ActiveDirectoryAPI")
     @mock.patch("coldfront.plugins.qumulo.services.allocation_service.async_task")
     def test_migrate_to_coldfront_with_contacts_missing(
         self,
         mock_async_task: mock.MagicMock,
+        mock_active_directory_api: mock.MagicMock,
         mock_itsm_client: mock.MagicMock,
     ) -> None:
         with open(
