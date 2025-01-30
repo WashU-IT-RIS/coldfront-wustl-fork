d037ea92369be619e03452546c243411794036de
Jim Harter <harterj@wustl.edu>
2025-01-30 16:51:26 -0600

    debug

--- coldfront/plugins/qumulo/static/filterable_checkbox_table_input.js
+++ coldfront/plugins/qumulo/static/filterable_checkbox_table_input.js
@@ -1,9 +1,9 @@
 class FilterableCheckboxTableInput {
-  constructor(widgetName, options, optionsPprint) {
+  constructor(widgetName, options, optionsJson, optionsPprint) {
     this.widgetName = widgetName;
-    console.log({ widgetName, options, optionsPprint });
-    this.options = JSON.parse(optionsPprint);
-
-    console.log({ options, parsed: this.options });
+    console.log({ widgetName, options, optionsJson, optionsPprint });
+    // this.options = JSON.parse(optionsPprint);
+    //
+    // console.log({ options, parsed: this.options });
   }
 }
