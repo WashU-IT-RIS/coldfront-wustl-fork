6ac6e34082393e19a5b00f26d1101e68ac7c55de
Jim Harter <harterj@wustl.edu>
2025-01-31 13:07:58 -0600

    generative options table

--- coldfront/plugins/qumulo/static/filterable_checkbox_table_input.js
+++ coldfront/plugins/qumulo/static/filterable_checkbox_table_input.js
@@ -9,6 +9,44 @@ class FilterableCheckboxTableInput {
       document.getElementById("checkbox-table-columns").textContent
     );
 
+    this.populateOptions();
+
     console.log({ widgetName, options: this.options });
   }
+
+  populateOptions() {
+    const tbody = document.getElementById(`${this.widgetName}-options-tbody`);
+
+    for (const option of this.options) {
+      const tr = this.getOptionsRowElement(option);
+      tbody.appendChild(tr);
+    }
+  }
+
+  getOptionsRowElement = (option) => {
+    const tr = document.createElement("tr");
+    tr.setAttribute("class", "text-nowrap");
+
+    const checkbox_td = document.createElement("td");
+    const checkboxInput = document.createElement("input");
+    checkboxInput.setAttribute("type", "checkbox");
+    checkboxInput.setAttribute("id", `${this.widgetName}-${option["value"]}`);
+    checkboxInput.setAttribute("value", option["value"]);
+    checkbox_td.appendChild(checkboxInput);
+
+    for (const value of Object.values(Object.label)) {
+      const td = this.getOptionsColumnElement(value);
+      tr.appendChild(td);
+    }
+
+    return tr;
+  };
+
+  getOptionsColumnElement = (value) => {
+    const td = document.createElement("td");
+    td.setAttribute("class", "text-nowrap");
+    td.appendChild(document.createTextNode(value));
+
+    return td;
+  };
 }
