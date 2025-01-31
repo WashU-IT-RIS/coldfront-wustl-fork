65d8df7dd36040f2a3a5d47d460d5e279a93b427
Jim Harter <harterj@wustl.edu>
2025-01-31 10:46:13 -0600

    debug

--- coldfront/plugins/qumulo/static/filterable_checkbox_table_input.js
+++ coldfront/plugins/qumulo/static/filterable_checkbox_table_input.js
@@ -1,6 +1,6 @@
 class FilterableCheckboxTableInput {
   constructor(widgetName, optionsName) {
-    const options = document.getElementById("optionsName").textContent;
+    const options = document.getElementById(optionsName).textContent;
     this.widgetName = widgetName;
     console.log({ widgetName, options: options });
     this.options = JSON.parse(options);
