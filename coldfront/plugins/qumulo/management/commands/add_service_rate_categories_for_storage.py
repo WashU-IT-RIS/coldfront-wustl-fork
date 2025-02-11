e23f4a760a2ba897e1c7d6d43ff91c67a105028c
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-02-11 14:31:48 -0400

    ITDEV-35266 add rate categories for storage

--- coldfront/plugins/qumulo/management/commands/add_service_rate_categories_for_storage.py
+++ coldfront/plugins/qumulo/management/commands/add_service_rate_categories_for_storage.py
@@ -10,69 +10,109 @@ from coldfront.core.service.models import (
 
 class Command(BaseCommand):
 
-    storage_2_service, _ = Service.objects.update_or_create(
-        name="Storage2", defaults={"description": "Storage 2 placeholder description"}
-    )
-    compute_2_service = Service.objects.update_or_create(
-        name="Compute2", defaults={"description": "Compute 2 placeholder description"}
-    )
+    def handle(self, *args, **options):
+        storage_2_service, _ = Service.objects.update_or_create(
+            name="Storage2",
+            defaults={"description": "Storage 2 placeholder description"},
+        )
+        compute_2_service = Service.objects.update_or_create(
+            name="Compute2",
+            defaults={"description": "Compute 2 placeholder description"},
+        )
 
-    comsumption_category, _ = ServiceRateCategory.objects.update_or_create(
-        service=storage_2_service,
-        model_name="consumption",
-        model_description="Consumption",
-        start_date=date(2025, 7, 1),
-        end_date=date(2026, 7, 1),
-    )
+        comsumption_category, _ = ServiceRateCategory.objects.update_or_create(
+            service=storage_2_service,
+            model_name="consumption",
+            model_description="Consumption",
+            start_date=date(2025, 7, 1),
+            end_date=date(2026, 7, 1),
+        )
 
-    condo_category, _ = ServiceRateCategory.objects.get_or_create(
-        service=storage_2_service,
-        model_name="condo",
-        model_description="Condo",
-        start_date=date(2025, 7, 1),
-        end_date=date(2026, 7, 1),
-    )
+        condo_category, _ = ServiceRateCategory.objects.get_or_create(
+            service=storage_2_service,
+            model_name="condo",
+            model_description="Condo",
+            start_date=date(2025, 7, 1),
+            end_date=date(2026, 7, 1),
+        )
 
-    subscription_category, _ = ServiceRateCategory.objects.update_or_create(
-        service=storage_2_service,
-        model_name="subscription",
-        model_description="Subscription",
-        start_date=date(2025, 7, 1),
-        end_date=date(2026, 7, 1),
-    )
+        subscription_category, _ = ServiceRateCategory.objects.update_or_create(
+            service=storage_2_service,
+            model_name="subscription",
+            model_description="Subscription",
+            start_date=date(2025, 7, 1),
+            end_date=date(2026, 7, 1),
+        )
 
-    ServiceRateCategoryTier.objects.update_or_create(
-        name="active",
-        rate="634",
-        unit_rate="100",
-        unit="TB",
-        cycle="month",
-        service_rate_category=subscription_category,
-    )
+        ServiceRateCategoryTier.objects.update_or_create(
+            name="active",
+            rate="634",
+            unit_rate="100",
+            unit="TB",
+            cycle="month",
+            service_rate_category=subscription_category,
+        )
 
-    ServiceRateCategoryTier.objects.update_or_create(
-        name="archive",
-        rate="264",
-        unit_rate="100",
-        unit="TB",
-        cycle="month",
-        service_rate_category=subscription_category,
-    )
+        ServiceRateCategoryTier.objects.update_or_create(
+            name="archive",
+            rate="264",
+            unit_rate="100",
+            unit="TB",
+            cycle="month",
+            service_rate_category=subscription_category,
+        )
 
-    ServiceRateCategoryTier.objects.update_or_create(
-        name="active",
-        rate="2643",
-        unit_rate="500",
-        unit="TB",
-        cycle="month",
-        service_rate_category=subscription_category,
-    )
+        ServiceRateCategoryTier.objects.update_or_create(
+            name="active",
+            rate="2643",
+            unit_rate="500",
+            unit="TB",
+            cycle="month",
+            service_rate_category=subscription_category,
+        )
 
-    ServiceRateCategoryTier.objects.update_or_create(
-        name="archive",
-        rate="881",
-        unit_rate="500",
-        unit="TB",
-        cycle="month",
-        service_rate_category=subscription_category,
-    )
+        ServiceRateCategoryTier.objects.update_or_create(
+            name="archive",
+            rate="881",
+            unit_rate="500",
+            unit="TB",
+            cycle="month",
+            service_rate_category=subscription_category,
+        )
+
+
+        ServiceRateCategoryTier.objects.update_or_create(
+            name="active",
+            rate="13",
+            unit_rate="1",
+            unit="TB",
+            cycle="month",
+            service_rate_category=comsumption_category,
+        )
+
+        ServiceRateCategoryTier.objects.update_or_create(
+            name="archive",
+            rate="5",
+            unit_rate="1",
+            unit="TB",
+            cycle="month",
+            service_rate_category=comsumption_category,
+        )
+
+        ServiceRateCategoryTier.objects.update_or_create(
+            name="active",
+            rate="529",
+            unit_rate="500",
+            unit="TB",
+            cycle="month",
+            service_rate_category=condo_category,
+        )
+
+        ServiceRateCategoryTier.objects.update_or_create(
+            name="archive",
+            rate="211",
+            unit_rate="500",
+            unit="TB",
+            cycle="month",
+            service_rate_category=condo_category,
+        )
