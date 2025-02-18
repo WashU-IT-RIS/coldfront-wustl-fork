74783f48ece4677d752d5c1786afa93f30e9c3c7
John Prewitt <japrewit@wustl.edu>
2025-02-18 14:52:41 -0600

    Initial commit - loosening up the validator

--- coldfront/plugins/qumulo/validators.py
+++ coldfront/plugins/qumulo/validators.py
@@ -143,12 +143,12 @@ def validate_single_ad_user_skip_admin(user: str):
 
 
 def validate_storage_name(value: str):
-    valid_character_match = re.match("^[0-9a-zA-Z\-_\.]*$", value)
+    valid_character_match = re.match("^[0-9a-zA-Z\-_\. \t]*$", value)
 
     if not valid_character_match:
         raise ValidationError(
             message=gettext_lazy(
-                "Storage name must contain only alphanumeric characters, hyphens, underscores, and periods."
+                "Storage name must contain only alphanumeric characters, hyphens, underscores, spaces, tabs, and periods."
             ),
             code="invalid",
         )
