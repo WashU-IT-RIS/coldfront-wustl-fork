7a2dd0baa9a949372db62f327440c360f40fa063
John Prewitt <japrewit@wustl.edu>
2025-02-19 20:20:01 -0600

    Initial commit - client list view

--- coldfront/plugins/qumulo/urls.py
+++ coldfront/plugins/qumulo/urls.py
@@ -5,6 +5,8 @@ from coldfront.plugins.qumulo.views import (
     update_allocation_view,
     create_sub_allocation_view,
     allocation_table_view,
+    client_list_view,
+    client_list_csv_view
 )
 
 app_name = "qumulo"
@@ -25,4 +27,14 @@ urlpatterns = [
         allocation_table_view.AllocationTableView.as_view(),
         name="allocation-table-list",
     ),
+    path(
+        "client-list",
+        client_list_view.ClientListView.as_view(),
+        name="client-list",
+    ),
+    path(
+        "client-export-csv",
+        client_list_csv_view.ClientListCSVView.as_view(),
+        name="client-export-csv",
+    )
 ]
