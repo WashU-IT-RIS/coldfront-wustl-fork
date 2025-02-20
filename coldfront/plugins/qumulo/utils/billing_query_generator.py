38d4e80d394008e2d9fa6efe33c227ba65a98d60
Caroline Jordan <jcaroline@wustl.edu>
2025-02-20 14:37:29 -0600

    adding subsidized columns

--- coldfront/plugins/qumulo/utils/billing_query_generator.py
+++ coldfront/plugins/qumulo/utils/billing_query_generator.py
@@ -134,6 +134,7 @@ class BillingGenerator:
                 {monthly_specific_columns}
                 report.rate rate,
                 report.service_unit unit
+                report.subsidized subsidized
             FROM (
                 SELECT
                     '{args["delivery_date"]}' delivery_date,
