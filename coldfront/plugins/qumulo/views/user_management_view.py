881258f6907b83180f767c5c3cc728c04ba8de80
Jim Harter <harterj@wustl.edu>
2025-02-26 16:27:03 -0600

    minor

--- coldfront/plugins/qumulo/views/user_management_view.py
+++ coldfront/plugins/qumulo/views/user_management_view.py
@@ -1,15 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import FormView
+from django.views.generic import TemplateView
 
 from coldfront.plugins.qumulo.forms.UserManagementForm import UserManagementForm
 
 import logging
 
 
-class UserManagementView(LoginRequiredMixin, FormView):
-    form_class = UserManagementForm
+class UserManagementView(LoginRequiredMixin, TemplateView):
     template_name = "user_management.html"
-
-    def form_valid(self, form):
-        logging.warning(f"UserManagementView form_valid: {form.cleaned_data}")
-        return super().form_valid(form)
