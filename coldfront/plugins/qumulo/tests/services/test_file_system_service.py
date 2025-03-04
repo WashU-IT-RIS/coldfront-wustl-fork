6e1a850114f1d6b73400a94902129bb9091c89f1
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-03-04 16:38:10 -0400

    ITDEV-36423 add service test

--- coldfront/plugins/qumulo/tests/services/test_file_system_service.py
+++ coldfront/plugins/qumulo/tests/services/test_file_system_service.py
@@ -1,6 +1,5 @@
 from unittest.mock import MagicMock, patch
 from coldfront.plugins.qumulo.services.file_system_service import FileSystemService
-from coldfront.plugins.qumulo.utils.qumulo_api import QumuloAPI
 
 PETABYTE_IN_BYTES = 1e15
 
@@ -12,23 +11,23 @@ class TestFileSystemService(TestCase):
 
     def setUp(self) -> None:
         self.mock_quota_response = {
-            "total_size": 10.11,
-            "free_size": 5.11,
-            "snapshot_size": 0.005,
+            "block_size_bytes": 4096,
+            "total_size_bytes": "5498921790996480",
+            "free_size_bytes": "1449206347350016",
+            "snapshot_size_bytes": "181465474080768",
+        }
+        self.expected_result = {
+            "total_size": 5.4989,
+            "free_size": 1.4492,
+            "snapshot_size": 0.1815,
         }
         return super().setUp()
 
-
-    @patch("coldfront.plugins.qumulo.services.QumuloAPI")
-    def test_get_file_system_stats(self, qumulo_api_mock: QumuloAPI) -> None:
+    @patch("coldfront.plugins.qumulo.services.file_system_service.QumuloAPI")
+    def test_get_file_system_stats(self, qumulo_api_mock: MagicMock) -> None:
         qumulo_api = MagicMock()
         qumulo_api.get_file_system_stats.return_value = self.mock_quota_response
         qumulo_api_mock.return_value = qumulo_api
-    
+
         actual_result = FileSystemService.get_file_system_stats()
-        expected_result = {
-            "total_size": 10.11,
-            "free_size": 5.11,
-            "snapshot_size": 0.005,
-        }
-        self.assertDictEqual(expected_result, actual_result)
+        self.assertDictEqual(self.expected_result, actual_result)
