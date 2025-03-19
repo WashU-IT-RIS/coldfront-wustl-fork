ac72aafd41cbf16adcac58090716bc3fd4611467
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-03-19 14:04:27 -0400

    ITDEV-35410 fix mock data

--- coldfront/plugins/qumulo/tests/services/test_file_quota_service.py
+++ coldfront/plugins/qumulo/tests/services/test_file_quota_service.py
@@ -13,7 +13,7 @@ load_dotenv(override=True)
 
 from django.test import TestCase
 
-from unittest import mock
+from unittest.mock import patch, MagicMock
 
 from coldfront.plugins.qumulo.services.file_quota_service import FileQuotaService
 
@@ -22,15 +22,21 @@ class TestFileQuotaService(TestCase):
 
     def setUp(self):
         create_metadata_for_testing()
+        self.mock_quota_response = get_mock_quota_response(
+            get_mock_quota_data("/storage2-dev/fs1"), "/storage2-dev/fs1"
+        )
         return super().setUp()
 
-    @mock.patch.dict(os.environ, {"QUMULO_RESULT_SET_PAGE_LIMIT": "2000"})
-    @mock.patch.dict(os.environ, {"ALLOCATION_LIMIT_THRESHOLD": "0.9"})
-    @mock.patch("coldfront.plugins.qumulo.services.file_quota_service.QumuloAPI")
-    def test_get_file_system_allocations_near_limit(self):
-        self.mock_quota_response = get_mock_quota_response(
-            get_mock_quota_data(self.STORAGE2_PATH), self.STORAGE2_PATH
-        )
+    @patch.dict(os.environ, {"QUMULO_RESULT_SET_PAGE_LIMIT": "2000"})
+    @patch.dict(os.environ, {"ALLOCATION_LIMIT_THRESHOLD": "0.9"})
+    @patch("coldfront.plugins.qumulo.services.file_quota_service.QumuloAPI")
+    def test_get_file_system_allocations_near_limit(
+        self, qumulo_api_mock: MagicMock
+    ) -> None:
+        qumulo_api = MagicMock()
+        qumulo_api.get_all_quotas_with_usage.return_value = self.mock_quota_response
+        qumulo_api_mock.return_value = qumulo_api
+
         allocations_near_limit = (
             FileQuotaService.get_file_system_allocations_near_limit()
         )
