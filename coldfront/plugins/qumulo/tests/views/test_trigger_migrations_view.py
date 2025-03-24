4b1c01912216bee81c6b88f8d128f0d56d6dee1b
Caroline Jordan <jcaroline@wustl.edu>
2025-03-24 11:35:53 -0700

    saving progress

--- coldfront/plugins/qumulo/tests/views/test_trigger_migrations_view.py
+++ coldfront/plugins/qumulo/tests/views/test_trigger_migrations_view.py
@@ -9,23 +9,27 @@ from coldfront.plugins.qumulo.views.trigger_migrations_view import TriggerMigrat
 from django.test import TestCase
 from django.urls.exceptions import NoReverseMatch
 from unittest.mock import patch, MagicMock
+from coldfront.plugins.qumulo.tests.fixtures import create_allocation_assets
 
 
 class TriggerMigrationsViewTests(TestCase):
+    def setUp(self) -> None:
+        create_allocation_assets()
+
     def testMigrationSuccessfulWithValidAllocation(self):
         success = True
-        valid_data = {"allocation_name_search": "/vol/rdcw-fs1/tychsen"}
+        valid_data = {"allocation_name_search": "/vol/rdcw-fs1/kchoi"}
         form = TriggerMigrationsForm(data=valid_data)
         form.is_valid()
         view = TriggerMigrationsView()
         try:
-            breakpoint()
             view.form_valid(form)
         except:
             success = False
         self.assertEqual(success, True)
 
     def testMigrationFailWithInvalidAllocation(self):
+        success = True
         invalid_data = {"allocation_name_search": "allocation"}
         form = TriggerMigrationsForm(data=invalid_data)
         form.is_valid()
@@ -33,5 +37,5 @@ class TriggerMigrationsViewTests(TestCase):
         try:
             view.form_valid(form)
         except:
-            pass
-        self.assertNotEqual(view.display_message, "Allocation metadata migrated")
+            success = False
+        self.assertEqual(success, False)
