5ae65c91d23911647fc51519e3eff92c8bbdb848
Caroline Jordan <jcaroline@wustl.edu>
2025-03-24 16:53:08 -0700

    mocks?

--- coldfront/plugins/qumulo/tests/views/test_trigger_migrations_view.py
+++ coldfront/plugins/qumulo/tests/views/test_trigger_migrations_view.py
@@ -1,8 +1,10 @@
+import os
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import TemplateView
 from coldfront.plugins.qumulo.forms import TriggerMigrationsForm
 
 from django.urls import reverse
+from unittest import mock
 
 from coldfront.plugins.qumulo.views.trigger_migrations_view import TriggerMigrationsView
 
@@ -16,7 +18,19 @@ class TriggerMigrationsViewTests(TestCase):
     def setUp(self) -> None:
         create_allocation_assets()
 
-    def testMigrationSuccessfulWithValidAllocation(self):
+    @mock.patch(
+        "coldfront.plugins.qumulo.services.itsm.migrate_to_coldfront.ItsmClient"
+    )
+    @mock.patch(
+        "coldfront.plugins.qumulo.services.allocation_service.ActiveDirectoryAPI"
+    )
+    @mock.patch("coldfront.plugins.qumulo.services.allocation_service.async_task")
+    def testMigrationSuccessfulWithValidAllocation(
+        self,
+        mock_async_task: mock.MagicMock,
+        mock_active_directory_api: mock.MagicMock,
+        mock_itsm_client: mock.MagicMock,
+    ):
         success = True
         valid_data = {"allocation_name_search": "/vol/rdcw-fs1/kchoi"}
         form = TriggerMigrationsForm(data=valid_data)
