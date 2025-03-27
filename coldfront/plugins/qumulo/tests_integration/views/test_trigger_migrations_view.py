f079a96b841a00274825de501659cf69fde770b1
Caroline Jordan <jcaroline@wustl.edu>
2025-03-27 13:12:43 -0700

    __

--- coldfront/plugins/qumulo/tests_integration/views/test_trigger_migrations_view.py
+++ coldfront/plugins/qumulo/tests_integration/views/test_trigger_migrations_view.py
@@ -1,9 +1,11 @@
 from coldfront.plugins.qumulo.forms import TriggerMigrationsForm
 from unittest.mock import patch, MagicMock
+from django.contrib import messages
 
 from coldfront.plugins.qumulo.views.trigger_migrations_view import TriggerMigrationsView
 
 from django.test import TestCase, tag, RequestFactory
+from coldfront.core.test_helpers.factories import UserFactory
 from coldfront.plugins.qumulo.tests.fixtures import create_allocation_assets
 
 
@@ -16,13 +18,17 @@ class TriggerMigrationsViewTests(TestCase):
         self,
     ):
         success = True
+
+        valid_data = {"allocation_name_search": "/vol/rdcw-fs1/kchoi"}
+        messages.add_message = MagicMock()
         request = RequestFactory().get(
             "src/coldfront.plugins.qumulo/views/trigger_migrations_view.py"
         )
-        valid_data = {"allocation_name_search": "/vol/rdcw-fs1/kchoi"}
+        user = UserFactory(is_staff=True)
         form = TriggerMigrationsForm(data=valid_data)
         form.is_valid()
         view = TriggerMigrationsView()
+        request.user = user
         view.request = request
         try:
             view.form_valid(form)
