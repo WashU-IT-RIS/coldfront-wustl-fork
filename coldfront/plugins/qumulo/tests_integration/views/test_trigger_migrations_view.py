9c738413e3b34bfc723137cb3a9683c8bf55bab8
Caroline Jordan <jcaroline@wustl.edu>
2025-03-27 14:38:13 -0700

    first integration tes passes

--- coldfront/plugins/qumulo/tests_integration/views/test_trigger_migrations_view.py
+++ coldfront/plugins/qumulo/tests_integration/views/test_trigger_migrations_view.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 
 from coldfront.plugins.qumulo.views.trigger_migrations_view import TriggerMigrationsView
 
-from django.test import TestCase, tag, RequestFactory
+from django.test import TestCase, tag, RequestFactory, Client
 from coldfront.core.test_helpers.factories import UserFactory
 from coldfront.plugins.qumulo.tests.fixtures import create_allocation_assets
 
@@ -18,17 +18,16 @@ class TriggerMigrationsViewTests(TestCase):
         self,
     ):
         success = True
-
-        valid_data = {"allocation_name_search": "/vol/rdcw-fs1/kchoi"}
-        messages.add_message = MagicMock()
+        messages.success = MagicMock()
         request = RequestFactory().get(
             "src/coldfront.plugins.qumulo/views/trigger_migrations_view.py"
         )
-        user = UserFactory(is_staff=True)
+        # user = UserFactory(is_staff=True)
+        valid_data = {"allocation_name_search": "/vol/rdcw-fs1/kchoi"}
         form = TriggerMigrationsForm(data=valid_data)
         form.is_valid()
         view = TriggerMigrationsView()
-        request.user = user
+        # request.user = user
         view.request = request
         try:
             view.form_valid(form)
