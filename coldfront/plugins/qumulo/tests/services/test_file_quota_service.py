72b464562c7acbfb6f31894d6baab7577486b32d
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-04-22 11:51:43 -0400

    ITDEV-35410 change paths

--- coldfront/plugins/qumulo/tests/services/test_file_quota_service.py
+++ coldfront/plugins/qumulo/tests/services/test_file_quota_service.py
@@ -33,31 +33,31 @@ class TestFileQuotaService(TestCase):
         create_metadata_for_testing()
         self.mock_quota_allocations = [
             {
-                "path": "/storage2-dev/fs1/near_limit/",
+                "path": "/storage2-dev/fs1/near_limit",
                 "id": "42080003",
                 "limit": "38482906972160",
                 "capacity_usage": "36558761623552",
             },
             {
-                "path": "/storage2-dev/fs1/over_limit/",
+                "path": "/storage2-dev/fs1/over_limit",
                 "id": "42130003",
                 "limit": "5497558138880",
                 "capacity_usage": "6497558138880",
             },
             {
-                "path": "/storage2-dev/fs1/under_limit/",
+                "path": "/storage2-dev/fs1/under_limit",
                 "id": "52929567",
                 "limit": "16492674416640",
                 "capacity_usage": "997732352",
             },
             {
-                "path": "/storage2-dev/fs1/just_inside_near_limit/",
+                "path": "/storage2-dev/fs1/just_inside_near_limit",
                 "id": "43010005",
                 "limit": "109951162777600",
                 "capacity_usage": "98956046499840",
             },
             {
-                "path": "/storage2-dev/fs1/at_the_limit/",
+                "path": "/storage2-dev/fs1/at_the_limit",
                 "id": "42030003",
                 "limit": "38482906972160",
                 "capacity_usage": "38482906972160",
