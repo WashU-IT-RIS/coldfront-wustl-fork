34c42e9c35f072cdd0e3b7f84c52b07ed4e8a179
Caroline Jordan <jcaroline@wustl.edu>
2025-05-13 11:32:13 -0700

    sorting actual list elements instead of values in array

--- coldfront/plugins/qumulo/static/multi_select_lookup_input.js
+++ coldfront/plugins/qumulo/static/multi_select_lookup_input.js
@@ -65,6 +65,7 @@ class MultiSelectLookupInput {
     if (existingMatches === null) {
       const liElement = this.createListItemElement(value);
       this.outputListElement.appendChild(liElement);
+      this.outputListElement.sort((a, b) => a.localeCompare(b));
 
       if (updateValue) {
         this.outputElement.value = this.addValueToOutputList(
