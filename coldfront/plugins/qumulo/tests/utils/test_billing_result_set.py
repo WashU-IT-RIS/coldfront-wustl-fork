7a0e6f03a4cf4b495cfaad97074252ffdc92e582
Caroline Jordan <jcaroline@wustl.edu>
2025-05-22 09:03:23 -0700

    subsidized fix

--- coldfront/plugins/qumulo/tests/utils/test_billing_result_set.py
+++ coldfront/plugins/qumulo/tests/utils/test_billing_result_set.py
@@ -42,15 +42,15 @@ class TestBillingResultSet(TestCase):
         new_alloc.status = AllocationStatusChoice.objects.get(name="Active")
         new_alloc.end_date = "2025-06-30"
         new_alloc.start_date = "2025-03-01"
+        new_alloc.save()
         subsidized_attribute = AllocationAttributeType.objects.get(
         name="subsidized"
-    )
+        )
         AllocationAttribute.objects.create(
             allocation=new_alloc,
             allocation_attribute_type=subsidized_attribute,
-            value="Yes",
+            value="No",
         )
-        new_alloc.save()
 
         out_of_date_alloc = create_allocation(
             project=self.project,
@@ -69,6 +69,7 @@ class TestBillingResultSet(TestCase):
 
     def test_monthly(self):
         listl = BillingResultSet.retrieve_billing_result_set("monthly", "2025-04-30 00:00:00", "2025-06-01 00:00:00")
+        breakpoint()
         count = len([l for l in listl if isinstance(l, dict)])
         expected_dict = {'billing_cycle': 'monthly', 'cost_center': 'Uncle Pennybags', 'subsidized': 'Yes', 'billing_exempt': 'No', 'pi': 'test', 'usage': 0.0}
         
--- coldfront/plugins/qumulo/utils/billing_result_set.py
+++ coldfront/plugins/qumulo/utils/billing_result_set.py
@@ -11,31 +11,31 @@ class BillingResultSet():
         date_format = "%Y-%m-%d %H:%M:%S"
         begin_billing_datetime = datetime.strptime(begin_billing, date_format)
         end_billing_datetime = datetime.strptime(end_billing, date_format)
-        allocation_list = Allocation.objects.filter(Q(resources__name="Storage2") & Q(start_date__gte=begin_billing_datetime) | Q(end_date__lte=end_billing_datetime))
+        allocation_list = Allocation.objects.filter(resources__name="Storage2", start_date__lte=begin_billing_datetime, end_date__gte=end_billing_datetime)
         
         billing_cycle_sub_query = AllocationAttribute.objects.filter(
         allocation=OuterRef("pk"), allocation_attribute_type__name="billing_cycle",
-        ).values("value")[:1]
+        ).values("value")
         cost_center_sub_query = AllocationAttribute.objects.filter(
         allocation=OuterRef("pk"), allocation_attribute_type__name="cost_center"
-        ).values("value")[:1]
+        ).values("value")
         subsidized_sub_query = AllocationAttribute.objects.filter(
         allocation=OuterRef("pk"), allocation_attribute_type__name="subsidized"
-        ).values("value")[:1]
+        ).values("value")
         billing_exempt_sub_query = AllocationAttribute.objects.filter(
         allocation=OuterRef("pk"), allocation_attribute_type__name="billing_exempt"
-        ).values("value")[:1]
-        pi_sub_query = Project.objects.filter(allocation__in=allocation_list).values('pi__username')[:1]
+        ).values("value")
+        pi_sub_query = Project.objects.filter(allocation__in=allocation_list).values('pi__username')
         usages_sub_query = AllocationAttributeUsage.history.filter(allocation_attribute__allocation__in=allocation_list, 
                                                          allocation_attribute__allocation_attribute_type__name="storage_quota", 
                                                          allocation_attribute__allocation__status__name="Active", 
-                                                         history_date__range=(make_aware(begin_billing_datetime), make_aware(end_billing_datetime))).values("value")[:1]
+                                                         history_date__range=(make_aware(begin_billing_datetime), make_aware(end_billing_datetime))).values("value")
         
         allocation_list = allocation_list.annotate(billing_cycle=Subquery(billing_cycle_sub_query), 
                                                    cost_center=Subquery(cost_center_sub_query), 
-                                                   subsidized=Subquery(subsidized_sub_query),
                                                    billing_exempt=Subquery(billing_exempt_sub_query),
+                                                   subsidized=Subquery(subsidized_sub_query),
                                                    pi=Subquery(pi_sub_query),
-                                                   usage=usages_sub_query).values('billing_cycle','cost_center','subsidized','billing_exempt','usage', 'pi')
+                                                   usage=Subquery(usages_sub_query)).values('billing_cycle','cost_center','subsidized','billing_exempt','usage', 'pi')
         
         return list(allocation_list)
