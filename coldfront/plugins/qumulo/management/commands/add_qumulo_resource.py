608277c7d9a6069bd1c77654569632bb8cebcb40
Caroline Jordan <jcaroline@wustl.edu>
2025-07-28 16:33:53 -0700

    storage_resource lsit

--- coldfront/plugins/qumulo/management/commands/add_qumulo_resource.py
+++ coldfront/plugins/qumulo/management/commands/add_qumulo_resource.py
@@ -7,28 +7,22 @@ class Command(BaseCommand):
         print("Adding Storage2/Qumulo resources")
         storage_resource_type = ResourceType.objects.get(name="Storage")
         acl_resource_type, created = ResourceType.objects.get_or_create(name="ACL")
+        storage_resource_list = [
+            {"name": "Storage2", "description": "Storage2 allocation via Qumulo"},
+            {"name": "Storage3", "description": "Storage3 allocation via Qumulo"},
+        ]
 
-        Resource.objects.get_or_create(
-            resource_type=storage_resource_type,
-            parent_resource=None,
-            name="Storage2",
-            description="Storage2 allocation via Qumulo",
-            is_available=True,
-            is_public=True,
-            is_allocatable=True,
-            requires_payment=True,
-        )
-
-        Resource.objects.get_or_create(
-            resource_type=storage_resource_type,
-            parent_resource=None,
-            name="Storage3",
-            description="Storage3 allocation via Qumulo",
-            is_available=True,
-            is_public=True,
-            is_allocatable=True,
-            requires_payment=True,
-        )
+        for storage_resource in storage_resource_list:
+            Resource.objects.get_or_create(
+                resource_type=storage_resource_type,
+                parent_resource=None,
+                name=storage_resource["name"],
+                description=storage_resource["description"],
+                is_available=True,
+                is_public=True,
+                is_allocatable=True,
+                requires_payment=True,
+            )
 
         Resource.objects.get_or_create(
             name="rw",
