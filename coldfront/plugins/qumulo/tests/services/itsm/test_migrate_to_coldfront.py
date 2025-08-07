adde5e94b503aa66a048e9b8148582e9026a2a80
Jim Harter <harterj@wustl.edu>
2025-08-07 15:49:52 -0500

    migrate to coldfront and merges

--- coldfront/plugins/qumulo/tests/services/itsm/test_migrate_to_coldfront.py
+++ coldfront/plugins/qumulo/tests/services/itsm/test_migrate_to_coldfront.py
@@ -1,24 +1,26 @@
-import os
 import json
 from django.test import TestCase
 
 from unittest import mock
+from unittest.mock import patch
 
 from coldfront.core.allocation.models import (
     Allocation,
     AllocationAttribute,
 )
 from coldfront.core.project.models import Project, ProjectAttribute
+
 from coldfront.plugins.qumulo.services.itsm.migrate_to_coldfront import (
     MigrateToColdfront,
 )
-
 from coldfront.plugins.qumulo.tests.fixtures import create_allocation_assets
+from coldfront.plugins.qumulo.tests.utils.mock_data import mock_qumulo_info
 
-QUMULO_INFO = json.loads(os.environ.get("QUMULO_INFO"))
+QUMULO_INFO = mock_qumulo_info
 STORAGE2_PATH = QUMULO_INFO["Storage2"]["path"]
 
 
+@patch.dict("os.environ", {"QUMULO_INFO": json.dumps(QUMULO_INFO)})
 class TestMigrateToColdfront(TestCase):
 
     def setUp(self) -> None:
