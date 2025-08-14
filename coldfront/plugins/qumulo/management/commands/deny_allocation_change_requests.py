520c67a5d7cd0a31260ff7105e64c448d2602c88
Caroline Jordan <jcaroline@wustl.edu>
2025-08-14 10:37:48 -0700

    this can be made simpler

--- coldfront/plugins/qumulo/management/commands/deny_allocation_change_requests.py
+++ coldfront/plugins/qumulo/management/commands/deny_allocation_change_requests.py
@@ -1,11 +1,13 @@
 from django.core.management.base import BaseCommand
-from coldfront.core.allocation.models import AllocationChangeRequest, AllocationAttributeChangeRequest
+from coldfront.core.allocation.models import AllocationChangeRequest, AllocationAttributeChangeRequest, AllocationChangeStatusChoice
 
 class Command(BaseCommand):
     help = 'Delete all pending allocation change requests from the database.'
 
     def handle(self, *args, **options):
-        pending_requests = AllocationChangeRequest.objects.filter(status__name='Pending')
+        pending_status = AllocationChangeStatusChoice.objects.get(name="Pending")
+        denied_status = AllocationChangeStatusChoice.objects.get(name="Denied")
+        pending_requests = AllocationChangeRequest.objects.filter(status=pending_status)
         count = pending_requests.count()
-        pending_requests.update(status__name='Denied')
+        pending_requests.update(status=denied_status)
         print(f"Denied {count} pending allocation change request(s).")
