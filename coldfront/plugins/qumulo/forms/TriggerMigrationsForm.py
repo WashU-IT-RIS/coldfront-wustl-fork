2d9125a50bd57bd837959bd1eade775471ffe2d0
Caroline Jordan <jcaroline@wustl.edu>
2025-10-03 20:54:06 -0700

    new field

--- coldfront/plugins/qumulo/forms/TriggerMigrationsForm.py
+++ coldfront/plugins/qumulo/forms/TriggerMigrationsForm.py
@@ -2,9 +2,17 @@ from django import forms
 
 
 class TriggerMigrationsForm(forms.Form):
+    allocation_resource_name = forms.ChoiceField(
+        label="Resource Name",
+        choices=[
+            ("Storage2", "Storage2"), ("Storage3", "Storage3")
+        ],
+        required=True,
+        help_text="Select the resource type for the allocation",
+    ),
     allocation_name_search = forms.CharField(
         label="Allocation Name",
         max_length=100,
         required=True,
-        help_text="Type the allocation name here!",
+        help_text="Type the allocation name here",
     )
