edaba903beb235b25c5aa5b6bf76ac26d4aea1b3
Caroline Jordan <jcaroline@wustl.edu>
2025-10-08 11:18:02 -0700

    remove extra parenthesis

--- coldfront/plugins/qumulo/forms/TriggerMigrationsForm.py
+++ coldfront/plugins/qumulo/forms/TriggerMigrationsForm.py
@@ -2,19 +2,15 @@ from django import forms
 
 
 class TriggerMigrationsForm(forms.Form):
-    allocation_name_search = (
-        forms.CharField(
-            label="Allocation Name",
-            max_length=100,
-            required=True,
-            help_text="Type the allocation name here",
-        ),
+    allocation_name_search = forms.CharField(
+        label="Allocation Name",
+        max_length=100,
+        required=True,
+        help_text="Type the allocation name here",
     )
-    allocation_resource_name = (
-        forms.ChoiceField(
-            label="Resource Name",
-            choices=[("Storage2", "Storage2"), ("Storage3", "Storage3")],
-            required=True,
-            help_text="Select the resource type for the allocation",
-        ),
+    allocation_resource_name = forms.ChoiceField(
+        label="Resource Name",
+        choices=[("Storage2", "Storage2"), ("Storage3", "Storage3")],
+        required=True,
+        help_text="Select the resource type for the allocation",
     )
