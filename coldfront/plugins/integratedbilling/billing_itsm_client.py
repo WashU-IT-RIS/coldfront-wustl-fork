0540be292ba5d89e200c75a1e300c5ceafd68e00
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-10-13 14:53:03 -0400

    ITDEV-37391 add billing itsm client with unit tests and integration tests

---
+++ coldfront/plugins/integratedbilling/billing_itsm_client.py
@@ -0,0 +1,49 @@
+import json
+from datetime import date, datetime
+from typing import Any
+
+from coldfront.plugins.qumulo.services.itsm.itsm_client_handler import ItsmClientHandler
+
+ITSM_ATTRIBUTES_FOR_BILLING = [
+    "id",
+    "sponsor",
+    "fileset_name",
+    "service_rate_category",
+    "amount_tb",
+    "funding_number",
+    "exempt",
+    "subsidized",
+    "is_condo_group",
+    "parent_id",
+    "quota",
+    "billing_cycle",
+    "status",
+    "provision_usage_creation_date",
+]
+
+
+# helper function to get the default billing date (first day of the current month)
+def _get_default_billing_date() -> date:
+    today = datetime.now().date()
+    return today.replace(day=1)
+
+
+class BillingItsmClient:
+    def __init__(self, billing_date: date = None):
+        self.handler = ItsmClientHandler()
+        self.billing_date = billing_date or _get_default_billing_date()
+
+    def get_billing_usages(self) -> list[dict[str, Any]]:
+        attributes = self.__get_attributes()
+        filters = self.__get_filters()
+        return self.handler.get_data(attributes, filters)
+
+    # Private methods
+    def __get_attributes(self) -> str:
+        return ",".join(ITSM_ATTRIBUTES_FOR_BILLING)
+
+    def __get_filters(self) -> str:
+        ITSM_ACTIVE_ALLOCATION_SERVICE_ID = 1
+        key = "provision_usage_creation_date"
+        billing_date = self.billing_date.strftime("%Y-%m-%d")
+        return f'{{"{key}":"{billing_date}","service_id":{ITSM_ACTIVE_ALLOCATION_SERVICE_ID}}}'
---
+++ coldfront/plugins/integratedbilling/tests_integration/test_integration_billing_itsm_client.py
@@ -0,0 +1,76 @@
+from datetime import datetime, timedelta
+import json
+from django.test import TestCase, tag
+
+from coldfront.plugins.integratedbilling.billing_itsm_client import BillingItsmClient
+from coldfront.plugins.integratedbilling.tests.fixtures import (
+    ITSM_ATTRIBUTES_FOR_BILLING,
+)
+
+
+class TestIntegrationBillingItsmClient(TestCase):
+
+    def setUp(self) -> None:
+        today = datetime.now().date()
+        self.default_billing_date = today.replace(day=1)
+        self.billing_itsm_client = BillingItsmClient()
+
+    @tag("integration")
+    def test_presence_of_billing_attributes(self):
+        billing_itsm_client = self.billing_itsm_client
+        empty_list = []
+        data = billing_itsm_client.get_billing_usages()
+        self.assertIsNot(data, empty_list)
+        service_provision_usage = data[0]
+        self.assertIsInstance(service_provision_usage, dict)
+        for key in ITSM_ATTRIBUTES_FOR_BILLING:
+            self.assertIn(key, service_provision_usage.keys())
+
+    def test_default_billing_attributes(self):
+        billing_itsm_client = self.billing_itsm_client
+        data = billing_itsm_client.get_billing_usages()
+        service_provision_usage = data[0]
+        for key in [
+            "provision_usage_creation_date",
+            "_service_provision",
+            "_provision_usage",
+        ]:
+            self.assertIn(key, service_provision_usage.keys())
+
+    @tag("integration")
+    def test_data_should_not_be_empty_for_current_month(self):
+        billing_itsm_client = self.billing_itsm_client
+        empty_list = []
+        data = billing_itsm_client.get_billing_usages()
+        self.assertIsNot(data, empty_list)
+        service_provision_usage = data[0]
+        self.assertIsInstance(service_provision_usage, dict)
+        self.assertIsNot(service_provision_usage, {})
+
+    @tag("integration")
+    def test_billing_date_is_first_of_month(self):
+        billing_itsm_client = self.billing_itsm_client
+        data = billing_itsm_client.get_billing_usages()
+        service_provision_usage = data[0]
+        provision_usage_creation_date = datetime.fromisoformat(
+            service_provision_usage.get("provision_usage_creation_date").replace(
+                "Z", "+00:00"
+            )
+        ).date()
+        self.assertEqual(self.default_billing_date, provision_usage_creation_date)
+
+    @tag("integration")
+    def test_data_should_not_be_empty_for_previous_month_(self):
+        first_day_previous_month_date = (
+            self.default_billing_date.replace(day=1) - timedelta(days=1)
+        ).replace(day=1)
+        billing_itsm_client = BillingItsmClient(first_day_previous_month_date)
+        data = billing_itsm_client.get_billing_usages()
+        service_provision_usage = data[0]
+        provision_usage_creation_date = datetime.fromisoformat(
+            service_provision_usage.get("provision_usage_creation_date").replace(
+                "Z", "+00:00"
+            )
+        ).date()
+        self.assertNotEqual(self.default_billing_date, provision_usage_creation_date)
+        self.assertEqual(first_day_previous_month_date, provision_usage_creation_date)
