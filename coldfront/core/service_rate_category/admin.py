f2db7be9c6490631896c204e1ea18270227e29c3
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-10-15 15:07:30 -0400

    ITDEV-35266 add better reference to parent's name; take 3

--- coldfront/core/service_rate_category/admin.py
+++ coldfront/core/service_rate_category/admin.py
@@ -16,14 +16,26 @@ class ServiceAdmin(admin.ModelAdmin):
 class ServiceRateCategoryAdmin(admin.ModelAdmin):
     list_display = ("service_name", "model_name", "start_date", "end_date")
 
+    def service_name(self, obj):
+        return obj.service.name
+
+    service_name.admin_order_field = "service__name"
+    service_name.short_description = "Service Name"
+
 
 @admin.register(ServiceRateCategoryTier)
 class ServiceRateCategoryTierAdmin(admin.ModelAdmin):
     list_display = (
         "name",
-        "service_rate_category_model_name",
+        "service_rate_category",
         "rate",
         "unit_rate",
         "unit",
         "cycle",
     )
+
+    def service_rate_category_name(self, obj):
+        return obj.service_rate_category.model_name
+
+    service_rate_category_name.admin_order_field = "service_rate_category__model_name"
+    service_rate_category_name.short_description = "Service Rate Category"
