21f5351f1876106bdac054df1e202ec4f8158fd9
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-10-21 00:52:27 -0400

    ITDEV-37389 test coverage for allocation query sets

--- coldfront/plugins/qumulo/tests/fixtures.py
+++ coldfront/plugins/qumulo/tests/fixtures.py
@@ -1,29 +1,152 @@
+from typing import Callable, Tuple, Union
+
+from coldfront.core.allocation.models import Allocation
+from coldfront.core.project.models import Project, ProjectAttributeType
 from coldfront.core.test_helpers.factories import (
     AAttributeTypeFactory,
+    AllocationAttributeFactory,
     AllocationAttributeTypeFactory,
     AllocationStatusChoiceFactory,
     AllocationUserStatusChoiceFactory,
     FieldOfScienceFactory,
+    ProjectAttributeFactory,
     ProjectAttributeTypeFactory,
     ProjectStatusChoiceFactory,
+    ProjectUserFactory,
     ProjectUserRoleChoiceFactory,
     ProjectUserStatusChoiceFactory,
     PAttributeTypeFactory,
     ResourceFactory,
+    ResourceTypeFactory,
+    AllocationUserFactory,
 )
 
 from coldfront.core.test_helpers.factories import field_of_science_provider
+from coldfront.plugins.qumulo.tests.helper_classes.factories import (
+    RisProjectFactory,
+    ReadOnlyGroupFactory,
+    ReadWriteGroupFactory,
+    Storage2Factory,
+    Storage3Factory,
+)
 
 
-def create_allocation_assets() -> None:
-    field_of_science_provider.add_element("Other")
-    FieldOfScienceFactory(description="Other")
+def create_metadata_for_testing() -> None:
+    create_attribute_types_for_ris_allocations()
+    create_project_choices()
+    create_allocation_choices()
+    create_ris_resources()
+
+
+def create_ris_project_and_allocations_storage2(
+    path: str,
+) -> Tuple[Project, list[Allocation]]:
+    return __create_ris_project_and_allocations(Storage2Factory, path)
+
+
+def create_ris_project_and_allocations_storage3(
+    path: str,
+) -> Tuple[Project, list[Allocation]]:
+    return __create_ris_project_and_allocations(Storage3Factory, path)
+
+
+def create_project_choices() -> None:
     ProjectStatusChoiceFactory(name="New")
     ProjectUserRoleChoiceFactory(name="Manager")
     ProjectUserStatusChoiceFactory(name="Active")
+
+
+def create_allocation_choices() -> None:
+    FieldOfScienceFactory(description="Other")
     AllocationStatusChoiceFactory(name="Pending")
+    AllocationStatusChoiceFactory(name="Active")
     AllocationUserStatusChoiceFactory(name="Active")
-    allocation_attribute_names = [
+
+
+def create_ris_resources() -> None:
+    ResourceFactory(
+        name="Storage2",
+        resource_type=ResourceTypeFactory(name="Storage"),
+        description="Storage allocation via Qumulo",
+    )
+    ResourceFactory(
+        name="Storage3",
+        resource_type=ResourceTypeFactory(name="Storage"),
+        description="Storage allocation via Qumulo",
+    )
+    ResourceFactory(
+        name="rw", resource_type=ResourceTypeFactory(name="ACL"), description="RW ACL"
+    )
+    ResourceFactory(
+        name="ro", resource_type=ResourceTypeFactory(name="ACL"), description="RO ACL"
+    )
+
+
+def create_attribute_types_for_ris_allocations() -> None:
+    _add_field_of_science_options_to_provider(["Other"])
+    _create_acl_allocation_attribute_names()
+    _create_storage_allocation_attribute_types()
+    _create_project_attribute_types()
+
+
+def __create_ris_project_and_allocations(
+    storage_factory: Callable[[], Union[Storage2Factory, Storage3Factory]],
+    path: str,
+) -> Tuple[Project, list[Allocation]]:
+    project = RisProjectFactory()
+
+    ProjectUserFactory(
+        project=project,
+        user=project.pi,
+        role=ProjectUserRoleChoiceFactory(name="Manager"),
+        status=ProjectUserStatusChoiceFactory(name="New"),
+    )
+
+    # django_get_or_create was not implemented in the core, so get the desired object
+    # TODO implement a RIS specific factory
+    sponsor_department_number = ProjectAttributeType.objects.get(
+        name="sponsor_department_number"
+    )
+    ProjectAttributeFactory(
+        project=project,
+        proj_attr_type=sponsor_department_number,
+    )
+
+    storage_allocation = storage_factory(project=project)
+
+    AllocationAttributeFactory(
+        allocation=storage_allocation,
+        allocation_attribute_type__name="storage_filesystem_path",
+        value=path,
+    )
+
+    AllocationUserFactory(
+        allocation=storage_allocation,
+        user=project.pi,
+        status=AllocationUserStatusChoiceFactory(name="Active"),
+    )
+
+    rw_group = ReadWriteGroupFactory(project=project)
+    AllocationUserFactory.create_batch(
+        2,
+        allocation=rw_group,
+        status=AllocationUserStatusChoiceFactory(name="Active"),
+    )
+
+    ro_group = ReadOnlyGroupFactory(project=project)
+    AllocationUserFactory(
+        allocation=ro_group,
+        status=AllocationUserStatusChoiceFactory(name="Active"),
+    )
+    return (
+        project,
+        [storage_allocation, rw_group, ro_group],
+    )
+
+
+def _create_storage_allocation_attribute_types() -> None:
+
+    storage_allocation_attribute_names = [
         ("storage_name", "Text"),
         ("storage_ticket", "Text"),
         ("storage_quota", "Int"),
@@ -52,16 +175,18 @@ def create_allocation_assets() -> None:
         ("prepaid_time", "Int"),
         ("prepaid_billing_date", "Date"),
     ]
-    for (
-        allocation_attribute_name,
-        allocation_attribute_type,
-    ) in allocation_attribute_names:
+    _create_allocation_attribute_types(storage_allocation_attribute_names)
 
-        AllocationAttributeTypeFactory(
-            name=allocation_attribute_name,
-            attribute_type=AAttributeTypeFactory(name=allocation_attribute_type),
-        )
 
+def _create_acl_allocation_attribute_names() -> None:
+    acl_allocation_attribute_names = [
+        ("storage_acl_name", "Text"),
+        ("storage_allocation_pk", "Int"),
+    ]
+    _create_allocation_attribute_types(acl_allocation_attribute_names)
+
+
+def _create_project_attribute_types() -> None:
     project_attribute_names = [
         ("is_condo_group", "Yes/No"),
         ("sponsor_department_number", "Text"),
@@ -73,6 +198,19 @@ def create_allocation_assets() -> None:
             attribute_type=PAttributeTypeFactory(name=project_attribute_type),
         )
 
-    ResourceFactory(name="Storage2")
-    ResourceFactory(name="rw")
-    ResourceFactory(name="ro")
+
+def _add_field_of_science_options_to_provider(options: list) -> None:
+    for option in options:
+        field_of_science_provider.add_element(option)
+
+
+def _create_allocation_attribute_types(attribute_types: list[Tuple[str, str]]) -> None:
+    for (
+        allocation_attribute_name,
+        allocation_attribute_type,
+    ) in attribute_types:
+
+        AllocationAttributeTypeFactory(
+            name=allocation_attribute_name,
+            attribute_type=AAttributeTypeFactory(name=allocation_attribute_type),
+        )
---
+++ coldfront/plugins/qumulo/tests/helper_classes/factories.py
@@ -0,0 +1,134 @@
+import factory
+
+from factory.django import DjangoModelFactory
+
+from coldfront.core.allocation.models import Allocation
+from coldfront.core.project.models import ProjectAttribute
+from coldfront.core.resource.models import ResourceType, Resource
+from coldfront.core.test_helpers.factories import (
+    AllocationStatusChoiceFactory,
+    AllocationUserFactory,
+    AllocationUserStatusChoiceFactory,
+    FieldOfScienceFactory,
+    ProjectFactory,
+    ProjectStatusChoiceFactory,
+    ProjectUserRoleChoiceFactory,
+    ProjectUserStatusChoiceFactory,
+    ResourceFactory,
+    ResourceTypeFactory,
+    UserFactory,
+)
+
+
+class RisProjectFactory(ProjectFactory):
+    pi = factory.SubFactory(UserFactory)
+    title = factory.LazyAttribute(lambda project: project.pi.username)
+    description = factory.LazyAttribute(
+        lambda project: f"project for {project.pi.username}"
+    )
+    status = factory.SubFactory(ProjectStatusChoiceFactory, name="New")
+    field_of_science = factory.SubFactory(FieldOfScienceFactory, description="Other")
+    force_review = False
+    requires_review = True
+
+
+class RisAllocationFactory(DjangoModelFactory):
+    class Meta:
+        model = Allocation
+
+    justification = factory.Faker("sentence")
+    status = factory.SubFactory(AllocationStatusChoiceFactory)
+    project = factory.SubFactory(RisProjectFactory)
+    is_changeable = True
+
+    class Params:
+        storage = factory.Trait(
+            justification="",
+            status=factory.SubFactory(AllocationStatusChoiceFactory, name="Active"),
+        )
+
+        read_write_group = factory.Trait(
+            justification="RW Users",
+            status=factory.SubFactory(AllocationStatusChoiceFactory, name="Active"),
+        )
+
+        read_only_group = factory.Trait(
+            justification="RO Users",
+            status=factory.SubFactory(AllocationStatusChoiceFactory, name="Active"),
+        )
+
+
+class Storage2Factory(RisAllocationFactory):
+    storage = True
+
+    @factory.post_generation
+    def resources(self, create, extracted, **kwargs):
+        if not create:
+            return None
+
+        resources = extracted or (
+            ResourceFactory(
+                name="Storage2", resource_type=ResourceTypeFactory(name="Storage")
+            ),
+        )
+        self.resources.add(*resources)
+
+
+class Storage3Factory(RisAllocationFactory):
+    storage = True
+
+    @factory.post_generation
+    def resources(self, create, extracted, **kwargs):
+        if not create:
+            return None
+
+        resources = extracted or (
+            ResourceFactory(
+                name="Storage3", resource_type=ResourceTypeFactory(name="Storage")
+            ),
+        )
+        self.resources.add(*resources)
+
+
+class Storage3Factory(RisAllocationFactory):
+    storage = True
+
+    @factory.post_generation
+    def resources(self, create, extracted, **kwargs):
+        if not create:
+            return None
+
+        resources = extracted or (
+            ResourceFactory(
+                name="Storage3", resource_type=ResourceTypeFactory(name="Storage")
+            ),
+        )
+        self.resources.add(*resources)
+
+
+class ReadWriteGroupFactory(RisAllocationFactory):
+    read_write_group = True
+
+    @factory.post_generation
+    def resources(self, create, extracted, **kwargs):
+        if not create:
+            return None
+
+        resources = extracted or (
+            ResourceFactory(name="rw", resource_type=ResourceTypeFactory(name="ACL")),
+        )
+        self.resources.add(*resources)
+
+
+class ReadOnlyGroupFactory(RisAllocationFactory):
+    read_only_group = True
+
+    @factory.post_generation
+    def resources(self, create, extracted, **kwargs):
+        if not create:
+            return None
+
+        resources = extracted or (
+            ResourceFactory(name="rw", resource_type=ResourceTypeFactory(name="ACL")),
+        )
+        self.resources.add(*resources)
