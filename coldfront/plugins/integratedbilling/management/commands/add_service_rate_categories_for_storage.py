ea6539d0050013a3613fc27adad888153ac567de
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-11-04 02:26:24 -0400

    add defaults to service_rate_category

--- coldfront/plugins/integratedbilling/management/commands/add_service_rate_categories_for_storage.py
+++ coldfront/plugins/integratedbilling/management/commands/add_service_rate_categories_for_storage.py
@@ -9,25 +9,27 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         ServiceRateCategory.objects.update_or_create(
-            model_name="consumption",
-            model_description="Consumption",
             start_date=date(2025, 10, 1),
             end_date=date(2026, 7, 1),
-            tier_name="active",
             rate=6.50,
             unit_rate="1",
-            unit="TB",
-            cycle="monthly",
+            defaults={
+                "model_name": "consumption",
+                "cycle": "monthly",
+                "unit": "TB",
+                "tier_name": "active",
+            }
         )
 
         ServiceRateCategory.objects.update_or_create(
-            model_name="consumption",
-            model_description="Consumption",
             start_date=date(2025, 10, 1),
             end_date=date(2026, 7, 1),
-            tier_name="archive",
             rate=3.15,
             unit_rate="1",
-            unit="TB",
-            cycle="monthly",
+            defaults={
+                "model_name": "consumption",
+                "cycle": "monthly",
+                "unit": "TB",
+                "tier_name": "archive",
+            }
         )
