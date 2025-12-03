dbbeff04ac246cafc0c473fd221ce5ad718289ae
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-12-02 21:13:44 -0400

    ITDEV-37800 fix schduled task and other clean up work

--- coldfront/plugins/integratedbilling/management/commands/add_schedule_for_integrated_billing_reports.py
+++ coldfront/plugins/integratedbilling/management/commands/add_schedule_for_integrated_billing_reports.py
@@ -1,11 +1,11 @@
 import arrow
+from datetime import datetime, timezone
+
 from django.core.management.base import BaseCommand
-
 from django_q.tasks import schedule
-
 from django_q.models import Schedule
 
-from datetime import datetime, timezone
+from coldfront.plugins.integratedbilling.report_generator import ReportGenerator
 
 SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_3_00_AM = (
     arrow.utcnow().replace(day=2, hour=3, minute=0).format(arrow.FORMAT_RFC3339)
@@ -14,20 +14,23 @@ SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_3_00_AM = (
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **options):
-        print("Scheduling generating integrated monthly billing report")
+    def handle(self, *args, **options) -> None:
+        print("Scheduling task for generating integrated billing report monthly...")
         schedule(
             func="coldfront.plugins.integratedbilling.management.commands.add_schedule_for_integrated_billing_reports.generate_integrated_billing_report",
             name="Generate Integrated Monthly Billing Report",
             schedule_type=Schedule.MONTHLY,
             next_run=SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_3_00_AM,
         )
+        return None
 
 
 def generate_integrated_billing_report() -> None:
-    from coldfront.plugins.integratedbilling.report_generator import ReportGenerator
+    usage_date = datetime.now(tz=timezone.utc).replace(
+        day=1, hour=6, minute=0, second=0, microsecond=0
+    )
 
-    usage_date = datetime.now(timezone.utc).strftime("%Y-%m-%d")
     report_generator = ReportGenerator(usage_date)
     success = report_generator.generate()
-    print("Integrated Monthly Billing Report generation success:", success)
+    print("Integrated Monthly Billing Report generation success: ", success)
+    return None
