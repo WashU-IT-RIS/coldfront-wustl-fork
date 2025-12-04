5d41e124478575c7f9826de6573688cb4ba5c148
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2025-12-04 10:50:43 -0400

    ITSD-37591 Hackalicious! Just for the nelson lab's storage3 until we figure out a better way

--- parallel_reset_commands/constants.py
+++ parallel_reset_commands/constants.py
@@ -1,3 +1,3 @@
-STORAGE_2_PREFIX = "/storage2/fs1"
+STORAGE_2_PREFIX = "/storage3/fs1" # Hackalicious! Just for the nelson lab's storage3 until we figure out a better way.
 STORAGE_3_PREFIX = "/storage3/fs1"
 BATCH_SIZE=10000
