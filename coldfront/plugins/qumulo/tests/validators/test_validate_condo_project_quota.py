726cf3cdfc1acae5a91e1910cda236ed57018b95
Caroline Jordan <jcaroline@wustl.edu>
2026-01-07 21:18:37 -0800

    use default_data in tests

--- coldfront/plugins/qumulo/tests/validators/test_validate_condo_project_quota.py
+++ coldfront/plugins/qumulo/tests/validators/test_validate_condo_project_quota.py
@@ -7,7 +7,7 @@ from coldfront.core.allocation.models import (
     AllocationAttribute,
 )
 
-from coldfront.plugins.qumulo.tests.utils.mock_data import build_models
+from coldfront.plugins.qumulo.tests.utils.mock_data import build_models, default_form_data
 from coldfront.plugins.qumulo.services.allocation_service import AllocationService
 from coldfront.plugins.qumulo.views.allocation_view import AllocationView
 from coldfront.plugins.qumulo.validators import existing_project_quota, update_calculate_total_project_quotas, create_calculate_total_project_quotas, calculate_remaining_condo_quota
@@ -23,62 +23,57 @@ class CondoQuotaValidationTest(TestCase):
         self.user = build_data["user"]
 
         self.client.force_login(self.user)
-
-        self.form_data = {
-            "project_pk": self.project.id,
-            "storage_type": "Storage2",
-            "storage_filesystem_path": "foo",
-            "storage_export_path": "bar",
-            "storage_ticket": "ITSD-54321",
-            "storage_name": "baz",
-            "storage_quota": 7,
-            "protocols": ["nfs"],
-            "rw_users": ["test"],
-            "ro_users": ["test1"],
-            "cost_center": "Uncle Pennybags",
-            "billing_exempt": "No",
-            "department_number": "Time Travel Services",
-            "billing_cycle": "monthly",
-            "service_rate_category": "consumption",
-        }
     
     def test_calculate_remaining_condo_quota(self, mock_ActiveDirectoryValidator: MagicMock,mock_async_task: MagicMock,mock_ActiveDirectoryAPI: MagicMock,):
-        AllocationService.create_new_allocation(self.form_data, self.user)
-        AllocationService.create_new_allocation(self.form_data, self.user)
+        num_allocations = 2
+        for i in range(num_allocations):
+            form_data = default_form_data.copy()
+            form_data["project_pk"] = self.project.pk
+            form_data["storage_filesystem_path"] = f"test_path_{i}"
+            AllocationService.create_new_allocation(form_data, self.user)
 
-        remaining_quota = calculate_remaining_condo_quota(self.form_data["project_pk"])
+        remaining_quota = calculate_remaining_condo_quota(self.project.pk)
         self.assertEqual(remaining_quota, 986)
     
     def test_existing_condo_quota_with_allocs(self, mock_ActiveDirectoryValidator: MagicMock,mock_async_task: MagicMock,mock_ActiveDirectoryAPI: MagicMock,):
-        AllocationService.create_new_allocation(self.form_data, self.user)
-        AllocationService.create_new_allocation(self.form_data, self.user)
+        num_allocations = 2
+        for i in range(num_allocations):
+            form_data = default_form_data.copy()
+            form_data["project_pk"] = self.project.pk
+            form_data["storage_filesystem_path"] = f"test_path_{i}"
+            AllocationService.create_new_allocation(form_data, self.user)
 
-        total_quota = existing_project_quota(self.form_data["project_pk"])
+        total_quota = existing_project_quota(self.project.pk)
         self.assertEqual(total_quota, 14)
 
     def test_existing_condo_quota_no_allocs(self, mock_ActiveDirectoryValidator: MagicMock,mock_async_task: MagicMock,mock_ActiveDirectoryAPI: MagicMock,):
-        total_quota = existing_project_quota(self.form_data["project_pk"])
+        total_quota = existing_project_quota(self.project.pk)
         self.assertEqual(total_quota, 0)
     
     def test_sum_project_quotas(self, mock_ActiveDirectoryValidator: MagicMock,mock_async_task: MagicMock,mock_ActiveDirectoryAPI: MagicMock,):
-        AllocationService.create_new_allocation(self.form_data, self.user)
-        AllocationService.create_new_allocation(self.form_data, self.user)
-        AllocationService.create_new_allocation(self.form_data, self.user)
+        num_allocations = 3
+        for i in range(num_allocations):
+            form_data = default_form_data.copy()
+            form_data["project_pk"] = self.project.pk
+            form_data["storage_filesystem_path"] = f"test_path_{i}"
+            AllocationService.create_new_allocation(form_data, self.user)
 
         total_quotas = create_calculate_total_project_quotas(
-            self.form_data["project_pk"], self.form_data["storage_quota"]
+            self.project.pk, form_data["storage_quota"]
         )
 
         self.assertEqual(total_quotas, 28)
     
     def test_update_calculate_total_project_quotas_larger(self, mock_ActiveDirectoryValidator: MagicMock,mock_async_task: MagicMock,mock_ActiveDirectoryAPI: MagicMock,):
-        AllocationService.create_new_allocation(self.form_data, self.user)
-        AllocationService.create_new_allocation(self.form_data, self.user)
-        AllocationService.create_new_allocation(self.form_data, self.user)
-        AllocationService.create_new_allocation(self.form_data, self.user)
+        num_allocations = 4
+        for i in range(num_allocations):
+            form_data = default_form_data.copy()
+            form_data["project_pk"] = self.project.pk
+            form_data["storage_filesystem_path"] = f"test_path_{i}"
+            AllocationService.create_new_allocation(form_data, self.user)
 
-        current_quota = self.form_data["storage_quota"]
-        project_pk = self.form_data["project_pk"]
+        current_quota = form_data["storage_quota"]
+        project_pk = self.project.pk
         storage_quota = 25
 
         updated_total_quotas = update_calculate_total_project_quotas(project_pk, storage_quota, current_quota)
@@ -86,13 +81,15 @@ class CondoQuotaValidationTest(TestCase):
         self.assertEqual(updated_total_quotas, 46)
 
     def test_update_calculate_total_project_quotas_smaller(self, mock_ActiveDirectoryValidator: MagicMock,mock_async_task: MagicMock,mock_ActiveDirectoryAPI: MagicMock,):
-        AllocationService.create_new_allocation(self.form_data, self.user)
-        AllocationService.create_new_allocation(self.form_data, self.user)
-        AllocationService.create_new_allocation(self.form_data, self.user)
-        AllocationService.create_new_allocation(self.form_data, self.user)
+        num_allocations = 4
+        for i in range(num_allocations):
+            form_data = default_form_data.copy()
+            form_data["project_pk"] = self.project.pk
+            form_data["storage_filesystem_path"] = f"test_path_{i}"
+            AllocationService.create_new_allocation(form_data, self.user)
 
-        current_quota = self.form_data["storage_quota"]
-        project_pk = self.form_data["project_pk"]
+        current_quota = form_data["storage_quota"]
+        project_pk = self.project.pk
         storage_quota = 5
 
         updated_total_quotas = update_calculate_total_project_quotas(project_pk, storage_quota, current_quota)
