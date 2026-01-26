638b5790fd07669f092ae91ad6fef031a9dd0f6e
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2026-01-25 22:38:48 -0400

    ITDEV-37904 add constructor to migration form to allow field manipulations

--- coldfront/plugins/qumulo/forms/TriggerMigrationsForm.py
+++ coldfront/plugins/qumulo/forms/TriggerMigrationsForm.py
@@ -5,16 +5,28 @@ from coldfront.plugins.qumulo.constants import DEFAULT_STORAGE_TYPE
 
 
 class TriggerMigrationsForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["allocation_resource_name"].initial = Resource.objects.get(
+            name=DEFAULT_STORAGE_TYPE
+        ).name
+        self.fields["allocation_resource_name"].choices = [
+            (resource.name, resource.name)
+            for resource in Resource.objects.filter(
+                resource_type__name="Storage", is_available=True
+            )
+        ]
+
     allocation_name_search = forms.CharField(
         label="Allocation Name",
         max_length=100,
         required=True,
-        help_text="Type the allocation name here",
+        help_text="the ITSM allocation name to migrate",
     )
+
     allocation_resource_name = forms.ChoiceField(
         label="Resource Name",
-        choices=[("Storage2", "Storage2"), ("Storage3", "Storage3")],
         required=True,
-        help_text="Select the resource type for the allocation",
-        initial=Resource.objects.get(name=DEFAULT_STORAGE_TYPE).name,
+        help_text="the Coldfront storage resource name to migrate allocations into",
     )
