2cfaaef5785f6043db7bc5c96eb1b0ad9b78d578
Tz-Kai Kevin Lin <tz-kai.lin@wustl.edu>
2026-01-27 14:11:38 -0600

    ITDEV-37767; tested to retrieve arguments;

--- coldfront/plugins/qumulo/management/commands/add_scheduled_generate_storage_usage_report_monthly.py
+++ coldfront/plugins/qumulo/management/commands/add_scheduled_generate_storage_usage_report_monthly.py
@@ -1,15 +1,10 @@
 import arrow
 from django.core.management.base import BaseCommand
-
-from django_q.tasks import schedule
-
-from django_q.models import Schedule
-
-from datetime import datetime, timezone
-
-from coldfront.plugins.qumulo.reports.storage_usage_report import StorageUsageReport 
-
 from django.core.mail import send_mail
+from django_q.tasks import schedule
+from django_q.models import Schedule
+from datetime import datetime, timezone
+from coldfront.plugins.qumulo.reports.storage_usage_report import StorageUsageReport 
 
 SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_6_AM = (
     arrow.utcnow().replace(day=2, hour=6, minute=00).format(arrow.FORMAT_RFC3339)
@@ -19,33 +14,32 @@ SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_6_AM = (
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('--usage_date', type=str, default=datetime.now(timezone.utc).strftime("%Y-%m-01"), help='Usage date in YYYY-MM-01 format (default: first day of current month)')
         parser.add_argument('--school', type=str, default='ENG', help='School unit (default: ENG)')
         parser.add_argument('--email', type=str, help='Recipient email address(es) with comma separation')
 
     def handle(self, *args, **options):
-        usage_date = options['usage_date']
         school = options['school']
         email = options['email']
-        print(f"Scheduling generating storage2&3 usage report to {email} for school {school} with consumption on {usage_date}...")
+        print(f"Scheduling to generate storage2&3 monthly usage report to {email} for school {school}...")
         schedule(
             func="coldfront.plugins.qumulo.management.commands.add_scheduled_generate_storage_usage_report_monthly.generate_storage2_3_monthly_usage_report",
             name=f"Generate Storage2&3 Monthly Usage Report for {school}",
             schedule_type=Schedule.MONTHLY,
             next_run=SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_6_AM,
-            usage_date=usage_date,
             school=school,
             email=email,
-            kwargs={'usage_date': usage_date, 'school': school, 'email': email},
         )
 
 
-def generate_storage2_3_monthly_usage_report(usage_date: str, school: str, email: str = None, **kwargs):
+def generate_storage2_3_monthly_usage_report(
+        usage_date = datetime.now(timezone.utc).strftime("%Y-%m-01"),
+        school: str = 'ENG',
+        email: str = None, **kwargs
+    ):
     """
     Generate the Storage2&3 Monthly Usage Report for the given school and email it.
     """
-    print(f"Storage2&3 Monthly Usage Report for {school} has been scheduled to be sent via email to {email}.")
-    print(f"kwargs: Storage2&3 Monthly Usage Report for {school} {kwargs['kwargs']['school']} has been scheduled to be sent via email to {kwargs['kwargs']['email']}.")
+    print(f"Generating Storage2&3 Monthly Usage Report for {school} emailing to {email}.")
 
     report_generator = StorageUsageReport(usage_date=usage_date)
     usage_report = report_generator.generate_report_for_school(school)
@@ -64,7 +58,7 @@ def generate_storage2_3_monthly_usage_report(usage_date: str, school: str, email
             recipient_list,
             fail_silently=False,
         )
-        print(f"Storage2&3 Monthly Usage Report for {school} has been sent via email to {email}.")
+        print(f"Storage2&3 Monthly Usage Report for {school} with consumptions on {usage_date} has been sent via email to {email}.")
     else:
         print("No recipient email provided. Report not sent.")
 
