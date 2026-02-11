bc98ae90f977e935c9c903d3efb38678b5db75c4
Jim H <135862556+harterjwustl@users.noreply.github.com>
2026-02-11 14:44:35 -0600

    initial commit on batch activator (#155)
    * initial commit on batch activator
    
    * process parent allocations first
    
    * minor
    
    * check for parent allocation activation
    
    * whoops
    
    * removing dead test
    
    * undo changes

---
+++ coldfront/plugins/qumulo/management/commands/activate_storage_allocations.py
@@ -0,0 +1,85 @@
+from django.core.management.base import BaseCommand
+
+from coldfront.core.allocation.models import (
+    Allocation,
+    AllocationStatusChoice,
+    AllocationLinkage,
+)
+from coldfront.core.allocation.signals import allocation_activate
+
+
+class Command(BaseCommand):
+    help = "Activate specified storage allocations in 'New' status"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--allocations",
+            nargs="+",
+            type=int,
+            required=True,
+            help="List of storage allocation IDs to activate",
+        )
+
+    def handle(self, *args, **options):
+        failed_allocations = []
+
+        allocations_ids = options["allocations"]
+
+        parent_allocations = []
+        sub_allocations = []
+
+        for allocation_id in allocations_ids:
+            allocation_linkage = AllocationLinkage.objects.filter(
+                children__pk=allocation_id
+            )
+
+            if allocation_linkage.count() > 0:
+                sub_allocations.append(allocation_id)
+            else:
+                parent_allocations.append(allocation_id)
+
+        for index, allocations in enumerate([parent_allocations, sub_allocations]):
+            for allocation_id in allocations:
+                try:
+                    self._activate_allocation(allocation_id, is_child=index)
+                except ValueError as e:
+                    failed_allocations.append(
+                        {"allocation_id": allocation_id, "error": str(e)}
+                    )
+
+        if failed_allocations:
+            print("Some allocations failed to activate:")
+            for failure in failed_allocations:
+                print(
+                    f" - Allocation ID {failure['allocation_id']}: {failure['error']}"
+                )
+
+    def _activate_allocation(self, allocation_id: int, is_child: bool):
+        print(f"Activating storage allocation with ID: {allocation_id}")
+
+        try:
+            allocation = Allocation.objects.get(pk=allocation_id)
+        except Allocation.DoesNotExist:
+            print(f"Allocation with ID {allocation_id} does not exist.")
+            raise ValueError(f"Allocation with ID {allocation_id} does not exist.")
+
+        if allocation.status.name != "New":
+            raise ValueError(
+                f"Allocation with ID {allocation_id} is not in 'New' status."
+            )
+
+        if is_child:
+            allocation_linkage = AllocationLinkage.objects.get(
+                children__pk=allocation_id
+            )
+            parent_allocation = allocation_linkage.parent
+
+            if parent_allocation.status.name != "Active":
+                raise ValueError(
+                    f"Parent allocation with ID {parent_allocation.pk} is not in 'Active' status."
+                )
+
+        allocation.status = AllocationStatusChoice.objects.get(name="Active")
+        allocation.save()
+
+        allocation_activate.send(sender=self.__class__, allocation_pk=allocation.pk)
