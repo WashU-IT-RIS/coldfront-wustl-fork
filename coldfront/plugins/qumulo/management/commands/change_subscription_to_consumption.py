ff2e8b14d8c845d799b1c7dbafa8314c2f6b5299
Caroline Jordan <jcaroline@wustl.edu>
2026-02-27 16:31:27 -0800

    removed allocation pk selection, ran black formatter

--- coldfront/plugins/qumulo/management/commands/change_subscription_to_consumption.py
+++ coldfront/plugins/qumulo/management/commands/change_subscription_to_consumption.py
@@ -5,9 +5,16 @@ from coldfront.core.allocation.models import AllocationAttribute, Allocation
 class Command(BaseCommand):
 
     def change_subscription_to_consumption(self):
-        subscription_allocations = Allocation.objects.filter(status__name__in=['Active', 'New', 'Pending'],allocationattribute__allocation_attribute_type__name="service_rate_category", allocationattribute__value="subscription").values("pk")
-        subscription_service_rate_attributes = AllocationAttribute.objects.filter(allocation_attribute_type__name="service_rate_category", value="subscription").filter(allocation__in=subscription_allocations)
-        
+        subscription_allocations = Allocation.objects.filter(
+            status__name__in=["Active", "New", "Pending"],
+            allocationattribute__allocation_attribute_type__name="service_rate_category",
+            allocationattribute__value="subscription",
+        )
+        subscription_service_rate_attributes = AllocationAttribute.objects.filter(
+            allocation_attribute_type__name="service_rate_category",
+            value="subscription",
+        ).filter(allocation__in=subscription_allocations)
+
         subscription_service_rate_attributes.update(value="consumption")
 
     def handle(self, *args, **options):
