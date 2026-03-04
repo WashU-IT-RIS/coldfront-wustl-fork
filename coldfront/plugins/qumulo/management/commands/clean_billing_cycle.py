c5ce613dfec9f7733bfd5c1b96087893286b18fc
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2026-03-04 15:19:55 -0400

    ITDEV-38133 organize imports, replace ic with print, and add comments

--- coldfront/plugins/qumulo/management/commands/clean_billing_cycle.py
+++ coldfront/plugins/qumulo/management/commands/clean_billing_cycle.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand
 
 from coldfront.core.allocation.models import Allocation, AllocationAttribute
-from icecream import ic
 
 
 class Command(BaseCommand):
@@ -39,9 +38,9 @@ class Command(BaseCommand):
 def clean_billing_cycle(
     billing_cycle: str, allocation_ids: list[int], dry_run: bool
 ) -> None:
-    ic(allocation_ids)
-    ic(billing_cycle)
-    ic(dry_run)
+    print(f"{allocation_ids=}")
+    print(f"{billing_cycle=}")
+    print(f"{dry_run=}")
 
     allocation_to_be_cleaned = Allocation.objects.filter(
         id__in=allocation_ids, status__name__in=["New", "Active", "Pending"]
@@ -49,18 +48,20 @@ def clean_billing_cycle(
 
     before_values = allocation_to_be_cleaned.values_list("id", "status__name")
     if dry_run:
-        ic("Dry run enabled. The following billing cycles would be created:")
+        print("Dry run enabled. The following billing cycles would be created:")
         for allocation_id, status in before_values:
-            ic(f" - Allocation ID {allocation_id}: {status}")
+            print(f" - Allocation ID {allocation_id}: {status}")
         return None
 
     for allocation in allocation_to_be_cleaned:
+        # The get_or_create is used here to ensure that if the billing_cycle attribute already exists for an allocation,
+        # it won't be overwritten. This is a safety measure to prevent unintended data changes.
         AllocationAttribute.objects.get_or_create(
             allocation=allocation,
             allocation_attribute_type_name="billing_cycle",
             defaults={"value": billing_cycle},
         )
 
-    ic("Billing cycles have been cleaned for the following allocations:")
-    for allocation_id in allocation_to_be_cleaned.values_list("id"):
-        ic(f' - Allocation ID {allocation_id}: "{billing_cycle}"')
+    print("Billing cycles have been created for the following allocations:")
+    for allocation_id in allocation_to_be_cleaned.values_list("id", flat=True):
+        print(f' - Allocation ID {allocation_id}: "{billing_cycle}"')
