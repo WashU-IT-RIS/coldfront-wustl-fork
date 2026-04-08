853bcef5015a1ba87391ee26eab704ddb5f604b0
Tz-Kai Kevin Lin <tz-kai.lin@wustl.edu>
2026-04-08 10:45:11 -0500

    ITDEV-38208; added to generate reports for Storage Active and Storage Archive;

--- coldfront/plugins/integratedbilling/management/commands/add_schedule_to_generate_monthly_storage_usage_report.py
+++ coldfront/plugins/integratedbilling/management/commands/add_schedule_to_generate_monthly_storage_usage_report.py
@@ -1,10 +1,11 @@
 import arrow
 from django.core.management.base import BaseCommand
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from django_q.tasks import schedule
 from django_q.models import Schedule
 from datetime import datetime, timezone
 from coldfront.plugins.integratedbilling.storage_usage_report import StorageUsageReport
+from coldfront.plugins.integratedbilling.constants import ServiceTiers
 
 SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_6_AM = (
     arrow.utcnow().replace(day=2, hour=6, minute=00).format(arrow.FORMAT_RFC3339)
@@ -14,23 +15,14 @@ SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_6_AM = (
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            "--tier",
-            type=str,
-            default="1",
-            help="Service Tier(default: 1, options: Active-- 1, Archive-- 2)",
-        )
         parser.add_argument("--email", type=str, help="Recipient email address")
 
     def handle(self, *args, **options):
-        tier = options["tier"]
         email = options["email"]
-        print(
-            f"Scheduling to generate Monthly Storage Active Usage Report to {email}..."
-        )
+        print(f"Scheduling to generate Monthly Storage Usage Report to {email}...")
         schedule(
             func="coldfront.plugins.integratedbilling.management.commands.add_schedule_to_generate_monthly_storage_usage_report.generate_monthly_storage_usage_report",
-            name="Generate Monthly Storage Active Usage Report",
+            name="Generate Monthly Storage Usage Report",
             schedule_type=Schedule.MONTHLY,
             next_run=SCHEDULED_FOR_2ND_DAY_OF_MONTH_AT_6_AM,
             email=email,
@@ -43,18 +35,20 @@ def generate_monthly_storage_usage_report(
     **kwargs,
 ):
     """
-    Generate the Monthly Storage Active Usage Report and email it.
+    Generate the Monthly Storage Usage Report and email it.
     """
-    print(f"Generating Monthly Storage Active Usage Report for emailing to {email}.")
-
-    report_agent = StorageUsageReport(
-        usage_date=usage_date, tier=kwargs.get("tier", "1")
+    usage_date_str = usage_date.strftime("%Y-%m-%d")
+    print(
+        f"Generating Monthly Storage Usage Report with consumptions on {usage_date_str} for emailing to {email}."
     )
-    usage_report = report_agent.generate_report()
-    print(usage_report)
 
-    subject = f"Monthly Storage Active Usage Report with consumptions on {usage_date}"
-    message = f"Here is the Monthly Storage Active Usage report with consumptions on {usage_date} by department per PIs:\n\n{usage_report}"
+    for tier in [ServiceTiers.Active, ServiceTiers.Archive]:
+        report_agent = StorageUsageReport(usage_date=usage_date, tier=tier)
+        usage_report = report_agent.generate_report()
+        usage_report = f"{usage_report}\n\n"
+
+    subject = f"Monthly Storage Usage Report with consumptions on {usage_date_str}"
+    message = f"Here is the Monthly Storage Usage report with consumptions on {usage_date_str} by department per PIs:\n\n{usage_report}"
     from_email = "noreply@gowustl.onmicrosoft.com"
     recipient_list = [email] if email else []
 
@@ -67,7 +61,7 @@ def generate_monthly_storage_usage_report(
             fail_silently=False,
         )
         print(
-            f"Monthly Storage Active Usage Report with consumptions on {usage_date} has been sent via email to {email}."
+            f"Monthly Storage Usage Report with consumptions on {usage_date_str} has been sent via email to {email}."
         )
     else:
         print("No recipient email provided. Report not sent.")
