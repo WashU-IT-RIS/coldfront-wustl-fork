c8e30d80a5233e92e5c4cbddd2d6d64b7ab3bc52
Tz-Kai Kevin Lin <tz-kai.lin@wustl.edu>
2026-04-08 13:57:04 -0500

    ITDEV-38208; added DEFAULT_FROM_EMAIL;

--- coldfront/config/settings.py
+++ coldfront/config/settings.py
@@ -4,26 +4,26 @@ from coldfront.config.env import ENV, PROJECT_ROOT
 
 # ColdFront split settings
 coldfront_configs = [
-    'base.py',
-    'database.py',
-    'auth.py',
-    'logging.py',
-    'core.py',
-    'email.py',
+    "base.py",
+    "database.py",
+    "auth.py",
+    "logging.py",
+    "core.py",
+    "email.py",
 ]
 
 # ColdFront plugin settings
 plugin_configs = {
-    'PLUGIN_SLURM': 'plugins/slurm.py',
-    'PLUGIN_IQUOTA': 'plugins/iquota.py',
-    'PLUGIN_FREEIPA': 'plugins/freeipa.py',
-    'PLUGIN_SYSMON': 'plugins/system_montior.py',
-    'PLUGIN_XDMOD': 'plugins/xdmod.py',
-    'PLUGIN_AUTH_OIDC': 'plugins/openid.py',
-    'PLUGIN_AUTH_LDAP': 'plugins/ldap.py',
-    'PLUGIN_LDAP_USER_SEARCH': 'plugins/ldap_user_search.py',
-    'PLUGIN_QUMULO': 'plugins/qumulo.py',
-    'PLUGIN_INTEGRATEDBILLING': 'plugins/integratedbilling.py',
+    "PLUGIN_SLURM": "plugins/slurm.py",
+    "PLUGIN_IQUOTA": "plugins/iquota.py",
+    "PLUGIN_FREEIPA": "plugins/freeipa.py",
+    "PLUGIN_SYSMON": "plugins/system_montior.py",
+    "PLUGIN_XDMOD": "plugins/xdmod.py",
+    "PLUGIN_AUTH_OIDC": "plugins/openid.py",
+    "PLUGIN_AUTH_LDAP": "plugins/ldap.py",
+    "PLUGIN_LDAP_USER_SEARCH": "plugins/ldap_user_search.py",
+    "PLUGIN_QUMULO": "plugins/qumulo.py",
+    "PLUGIN_INTEGRATEDBILLING": "plugins/integratedbilling.py",
 }
 
 # This allows plugins to be enabled via environment variables. Can alternatively
@@ -35,20 +35,21 @@ for key, pc in plugin_configs.items():
 # Local settings overrides
 local_configs = [
     # Local settings relative to coldfront.config package
-    'local_settings.py',
-
+    "local_settings.py",
     # System wide settings for production deployments
-    '/etc/coldfront/local_settings.py',
-
+    "/etc/coldfront/local_settings.py",
     # Local settings relative to coldfront project root
-    PROJECT_ROOT('local_settings.py')
+    PROJECT_ROOT("local_settings.py"),
 ]
 
-if ENV.str('COLDFRONT_CONFIG', default='') != '':
+if ENV.str("COLDFRONT_CONFIG", default="") != "":
     # Local settings from path specified via environment variable
-    local_configs.append(environ.Path(ENV.str('COLDFRONT_CONFIG'))())
+    local_configs.append(environ.Path(ENV.str("COLDFRONT_CONFIG"))())
 
 for lc in local_configs:
     coldfront_configs.append(optional(lc))
 
 include(*coldfront_configs)
+
+# Default email address for outgoing mail
+DEFAULT_FROM_EMAIL = "noreply@gowustl.onmicrosoft.com"
