a3e802308323b35ff62556bf4a2d170ad2e8aac9
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2026-06-20 23:06:16 -0400

    ITDEV-38490 using template

--- coldfront/core/portal/views.py
+++ coldfront/core/portal/views.py
@@ -22,7 +22,6 @@ from coldfront.core.research_output.models import ResearchOutput
 def home(request):
 
     context = {}
-    context["EXTRA_APPS"] = settings.INSTALLED_APPS
 
     if request.user.is_authenticated:
         template_name = 'portal/authorized_home.html'
@@ -49,11 +48,11 @@ def home(request):
             pass
     else:
         # template_name = 'portal/nonauthorized_home.html'
-        # return redirect("login")
-        redirect_url = f"{reverse('login')}?next={reverse('home')}"
-        return redirect(redirect_url)
+        template_name = 'user/login.html'
+        context['next'] = reverse('portal:home')
+        context['form'] = None
 
-    # context['EXTRA_APPS'] = settings.INSTALLED_APPS
+    context['EXTRA_APPS'] = settings.INSTALLED_APPS
 
     if 'coldfront.plugins.system_monitor' in settings.INSTALLED_APPS:
         from coldfront.plugins.system_monitor.utils import get_system_monitor_context
