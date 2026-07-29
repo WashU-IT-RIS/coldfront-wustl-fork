165762a97d276270acbdfef1b327c29c9e1bb910
Brennan Mulligan <bmulligan@wustl.edu>
2026-07-29 13:18:37 -0500

    code cleanup, make a function out of http debugging

--- utils/auth_api_client.py
+++ utils/auth_api_client.py
@@ -15,13 +15,22 @@ from requests.auth import HTTPBasicAuth
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+# see: https://groups.google.com/g/django-users/c/pQbRxuIeOzE
+
+def enable_http_debugging():
+    http_client.HTTPConnection.debuglevel = 1
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger('requests.packages.urllib3')
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
 class ColdfrontParser(HTMLParser):
     csrf_token = None
 
     def handle_starttag(self, tag, attrs):
         if tag.lower() != 'input':
             return
-        # print(f'Encountered a start tag: {tag}')
         for k, v in attrs:
             if k == 'type':
                 if v != 'hidden':
@@ -32,31 +41,15 @@ class ColdfrontParser(HTMLParser):
             if k == 'value':
                 self.csrf_token = v
 
-
     def handle_endtag(self, tag):
-        # print(f'Encountered an end tag {tag}')
         pass
 
     def handle_data(self, data):
-        # print(f'Encountered data {data}')
         pass
 
-# see: https://groups.google.com/g/django-users/c/pQbRxuIeOzE
-# curl -v -k -u 'user:pass' -e 'https://coldfront.ris.wustl.edu/user/login' 'https://coldfront.ris.wustl.edu/user/login' >/dev/null
-
-# http_client.HTTPConnection.debuglevel = 1
-# logging.basicConfig()
-# logging.getLogger().setLevel(logging.DEBUG)
-# requests_log = logging.getLogger('requests.packages.urllib3')
-# requests_log.setLevel(logging.DEBUG)
-# requests_log.propagate = True
-
-username = os.environ.get('API_USER', False)
-if not username:
-    user = input('Enter WUSTL key: ')
-password = os.environ.get('API_PASS', False)
-if not password:
-    password = getpass.getpass('Enter Password: ')
+username = os.environ.get('API_USER', False) or input('Enter WUSTL key: ')
+password = os.environ.get('API_PASS', False) or \
+    getpass.getpass('Enter Password: ')
 with requests.Session() as session:
     loginUrl = 'https://coldfront.ris.wustl.edu/user/login?next='
     loginResp = session.get(
@@ -68,10 +61,6 @@ with requests.Session() as session:
         sys.exit(1)
     parser = ColdfrontParser()
     parser.feed(loginResp.text)
-    # if parser.csrf_token:
-        # print(f'Found token {parser.csrf_token}')
-    # if session.cookies:
-        # print(f'Session had cookies: {session.cookies}')
     session.headers.update({
         'Content-type': 'application/x-www-form-urlencoded',
         'Referer': loginUrl
