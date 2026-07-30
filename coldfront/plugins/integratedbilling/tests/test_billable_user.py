9141fc1d14e3b68df7086b849b8dae0ed2614608
Santiago Álvarez Vargas <santiago.v.alvarez@gmail.com>
2026-07-30 04:52:04 -0400

    ITDEV-38372 expand tests

--- coldfront/plugins/integratedbilling/tests/test_billable_user.py
+++ coldfront/plugins/integratedbilling/tests/test_billable_user.py
@@ -1,38 +1,37 @@
-from unittest import skip
 from django.test import TestCase  
 
-# from coldfront.plugins.integratedbilling.factories import BillableUserFactory
 from coldfront.plugins.integratedbilling.subsidies import BillableUser
+from coldfront.plugins.qumulo.tests.fixtures import create_ris_project_and_allocations_storage3
 
 
 class TestBillableUser(TestCase):
-    @skip
+    def setUp(self):
+        # Set up any necessary test data or mocks here
+        pass
+    
     def test_factory(self):
         # Create a BillableUser instance using the factory
-        billable_user = BillableUser()
+        billable_user = BillableUser.factory("testuser")
         self.assertIsInstance(billable_user, BillableUser)
         self.assertIsNotNone(billable_user.get_user())
 
-    @skip
+    
     def test_is_eligible_for_subsidy(self):
         # Create a BillableUser instance using the factory
-        billable_user = BillableUser()
-        # Since the eligibility criteria is currently a placeholder, we expect this to return False
-        self.assertFalse(billable_user.is_eligible_for_subsidy())
+        billable_user = BillableUser.factory("testuser")
+        self.assertFalse(billable_user.is_eligible_for_subsidy())  # Assuming testuser is not faculty
 
-    @skip
+    
     def test_factory_by_allocation(self):
         # This test would require creating an Allocation instance and then using it to create a BillableUser
-        # For now, we will just assert that the factory method exists and can be called without error
-        from coldfront.core.billing.models import Allocation
-
-        allocation = (
-            Allocation()
-        )  # This would need to be properly instantiated with required fields
+        _, allocations = create_ris_project_and_allocations_storage3(
+            storage_filesystem_path="/storage3/fs1/testuser",
+        )
+        allocation = allocations["storage_allocation"]
         billable_user = BillableUser.factory_by_allocation(allocation)
         self.assertIsInstance(billable_user, BillableUser)
 
-    @skip
+    
     def test_str_representation(self):
         billable_user = BillableUser()
         str_representation = str(billable_user)
