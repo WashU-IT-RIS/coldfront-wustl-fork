d3b530d68997fcdcc1fe533b3fa1390e591de05f
Brennan Mulligan <bmulligan@wustl.edu>
2026-09-17 11:59:45 -0500

    actually add latest script

---
+++ utils/allocation_latest.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+
+from argparse import ArgumentParser
+
+class Latest:
+    path = None
+    modified = 0.0
+
+    def update(self, path, stat):
+        max_stat = max([stat.st_atime, stat.st_mtime, stat.st_ctime])
+        if max_stat >= self.modified:
+            return
+        self.path = path
+        self.modified = max_stat
+
+    def dump(self):
+        def time_conv():
+            return time.strftime(
+                '%m/%d/%Y - %H:%M:%S GMT',
+                time.gmtime(self.modified)
+            )
+        print(f'{self.path},{time_conv()}')
+
+ap = ArgumentParser(
+    description='Allocation "last used" tool'
+)
+ap.add_argument(
+    '-p',
+    '--path',
+    dest='path',
+    help='The path to inspect',
+    required=True
+)
+args = ap.parse_args()
+latest = Latest()
+for item in os.walk(args.path):
+    latest.update(item[0], os.stat(item[0]))
+    for dir_file in item[2]:
+        file_path = os.path.join(item[0], dir_file)
+        if os.path.islink(file_path):
+            continue
+        latest.update(file_path, os.stat(file_path))
+latest.dump()
+sys.exit(0)
